8c1a577792220acac3348d086fbf1fb3c6cb81d1
BL146U <bl146u@gmail.com>
2021-04-21 19:17:16 +0300

    Changes

--- assets/js/project/modeling.js
+++ assets/js/project/modeling.js
@@ -4,6 +4,9 @@
 (($) => {
 
 
+    let terra_toolbar, terra_board, terra_params;
+    
+
     let LoadModel = $("#modal-window-load-model").ModalWindow({
         title:window.Messages.get("LOAD_MODEL"),
         width:680,
