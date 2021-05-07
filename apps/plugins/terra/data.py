f831d7d07f1216b15cd22658d0f1173f1544eadb
BL146U <bl146u@gmail.com>
2021-05-07 14:31:36 +0300

    Change

--- apps/plugins/terra/data.py
+++ apps/plugins/terra/data.py
@@ -218,7 +218,6 @@ class TerraExchangeProject:
     datasets: dict
     tags: dict
     dataset: str
-    task: str
     model_name: str
     layers: LayerDict
     start_layers: LayerDict
@@ -236,7 +235,6 @@ class TerraExchangeProject:
         self.datasets = kwargs.get("datasets", {})
         self.tags = kwargs.get("tags", {})
         self.dataset = kwargs.get("dataset", "")
-        self.task = kwargs.get("task", "")
         self.model_name = kwargs.get("model_name", "")
         self.layers = kwargs.get("layers", LayerDict())
         self.start_layers = kwargs.get("start_layers", LayerDict())
@@ -259,7 +257,6 @@ class TerraExchangeProject:
     datasets     : {len(self.datasets.keys())}
     tags         : {len(self.tags.keys())}
     dataset      : {self.dataset}
-    task         : {self.task}
     model_name   : {self.model_name}
     layers       : {len(self.layers.items.keys())}
     start_layers : {len(self.start_layers.items.keys())}
@@ -274,7 +271,7 @@ class TerraExchangeProject:
 
     @property
     def dataset_selected(self) -> bool:
-        return self.dataset != "" and self.task != ""
+        return self.dataset != ""
 
     @property
     def as_json_string(self) -> dict:
