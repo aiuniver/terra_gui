c1894b08e1f7083c25e2e0f676a9eee97323a07d
BL146U <bl146u@gmail.com>
2021-05-08 22:15:50 +0300

    Changes

--- apps/api/views.py
+++ apps/api/views.py
@@ -6,5 +6,9 @@ from .data.exchange import ExchangeData
 
 class ExchangeAPIView(APIView):
     def post(self, request, *args, **kwargs):
+        print("-------------------------------")
+        print(request.headers.keys())
+        print(request.headers.get("X-Colab-State"))
+        print("-------------------------------")
         data = ExchangeData(name=kwargs.get("name"), data=request.data)
         return Response(data.__dict__)
