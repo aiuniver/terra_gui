95cbd28626645132bfcbdfe471ae70973168c6b0
SvyatS <prisenko2002@gmail.com>
2021-06-09 15:59:04 +0700

    added serializeJSON

--- assets/js/form-widgets.js
+++ assets/js/form-widgets.js
@@ -15,38 +15,41 @@
         let render_widget = {
 
             "input": () => {
-                let type, widget;
+                let type, data_value_type, widget;
                 switch (options.type) {
                     case "int":
                     case "float":
                         type = "number";
+                        data_value_type = "number";
                         break;
                     case "bool":
                         type = "checkbox";
+                        data_value_type = "boolean";
                         break;
                     default:
                         type = "text";
+                        data_value_type = "string";
                         break;
                 }
                 if (type === "checkbox") {
                     widget = $(`
                         <label for="field_form-${name}">${label}</label>
                         <div class="checkout-switch">
-                            <input type="${type}" id="field_form-${name}" name="${name}"${options.default === true ? 'checked="checked"' : ''}${options.disabled ? ' disabled="disabled"' : ""}${options.readonly ? ' readonly="readonly"' : ""} />
+                            <input type="${type}" id="field_form-${name}" name="${name}"${options.default === true ? 'checked="checked"' : ''}${options.disabled ? ' disabled="disabled"' : ""}${options.readonly ? ' readonly="readonly"' : ""} data-value-type="${data_value_type}" data-unchecked-value="false"/>
                             <span class="switcher"></span>
                         </div>
                     `);
                 } else {
                     widget = $(`
                         <label for="field_form-${name}">${label}</label>
-                        <input type="${type}" id="field_form-${name}" name="${name}" value="${options.default === undefined || options.default === null ? '' : options.default}"${options.disabled ? ' disabled="disabled"' : ""}${options.readonly ? ' readonly="readonly"' : ""} />
+                        <input type="${type}" id="field_form-${name}" name="${name}" value="${options.default === undefined || options.default === null ? '' : options.default}"${options.disabled ? ' disabled="disabled"' : ""}${options.readonly ? ' readonly="readonly"' : ""} data-value-type="${data_value_type}"/>
                     `);
                 }
                 return widget_wrapper.append(widget);
             },
 
             "select": () => {
-                let select = $(`<select name="${name}" id="field_form-${name}" class="jquery-ui-menuselect"${options.disabled ? ' disabled="disabled"' : ""}></select>`);
+                let select = $(`<select name="${name}" id="field_form-${name}" class="jquery-ui-menuselect"${options.disabled ? ' disabled="disabled"' : ""}   data-value-type="string"></select>`);
                 for (let index in options.available) {
                     let option = options.available[index] || "";
                     select.append($(`
