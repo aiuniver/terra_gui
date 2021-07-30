131a193d6e8fb487ce835cdedded660aeda2ad5f
BL146U <bl146u@gmail.com>
2021-07-30 17:18:08 +0300

    -

---
+++ terra_ai/data/deploy/stages.py
@@ -0,0 +1,32 @@
+from typing import Optional
+from pydantic import validator
+from pydantic.types import PositiveInt
+from transliterate import slugify
+
+from terra_ai.data.mixins import BaseMixinData
+from terra_ai.data.types import confilename
+
+
+class StagePrepareUserData(BaseMixinData):
+    login: str
+    name: str
+    lastname: str
+
+
+class StagePrepareData(BaseMixinData):
+    stage: PositiveInt
+    user: StagePrepareUserData
+    project_name: str
+    project_name_lat: Optional[str]
+    url: str
+    replace: bool = False
+    filename: confilename(ext="zip")
+    filesize: PositiveInt
+
+    @validator("project_name_lat", always=True)
+    def _validate_project_name_lat(cls, value: str, values) -> str:
+        project_name = values.get("project_name")
+        if not project_name:
+            return value
+        value = slugify(project_name, language_code="ru")
+        return value
