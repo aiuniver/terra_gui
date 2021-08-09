a550c1b20467f87f4e7624889ceece05dea468de
BL146U <bl146u@gmail.com>
2021-08-09 11:35:23 +0300

    -

--- apps/plugins/frontend/presets/defaults.py
+++ apps/plugins/frontend/presets/defaults.py
@@ -46,7 +46,7 @@ for layer in Layer:
     params = getattr(types, layer.name)
     Defaults["modeling"]["layers_types"].update(
         {
-            layer.value: {
+            layer.name: {
                 "main": __get_layer_type_params(params.ParametersMainData, "main"),
                 "extra": __get_layer_type_params(params.ParametersExtraData, "extra"),
             }
