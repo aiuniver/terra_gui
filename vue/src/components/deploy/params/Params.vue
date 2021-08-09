27dea5b58533e53ab3babda32bd6dc0f49325e9d
SvyatS <prisenko2002@gmail.com>
2021-08-09 13:17:58 +0700

    init deploy store

--- vue/src/components/deploy/params/Params.vue
+++ vue/src/components/deploy/params/Params.vue
@@ -26,56 +26,7 @@
             </div>
           </div>
         </div>
-        <div class="load__answer">
-          <scrollbar>
-            <div class="terra-url">https://srv2.demo.neural-university.ru</div>
-            <div class="answer-url">
-              Чтобы получилось:
-              <span>
-              https://srv2.demo.neural-university.ru/media/check_od/output_files/34234452.jpg
-            </span>
-            </div>
-            <div class="module-list">
-              <div class="module-list__title">СПИСОК МОДУЛЕЙ:</div>
-              <div class="module-list__item">
-                <div class="item__title">Распознавание птиц на видео</div>
-                <div class="item__url">https://demo.neural-university.ru/birds_video/</div>
-                <div class="item__input">
-                  Вход:
-                  <span>od_content файл с изображением</span>
-                </div>
-                <div class="item__output">
-                  Выход:
-                  <span>Относительная ссылка</span>
-                </div>
-              </div>
-              <div class="module-list__item">
-                <div class="item__title">Распознавание птиц на видео</div>
-                <div class="item__url">https://demo.neural-university.ru/birds_video/</div>
-                <div class="item__input">
-                  Вход:
-                  <span>od_content файл с изображением</span>
-                </div>
-                <div class="item__output">
-                  Выход:
-                  <span>Относительная ссылка</span>
-                </div>
-              </div>
-              <div class="module-list__item">
-                <div class="item__title">Распознавание птиц на видео</div>
-                <div class="item__url">https://demo.neural-university.ru/birds_video/</div>
-                <div class="item__input">
-                  Вход:
-                  <span>od_content файл с изображением</span>
-                </div>
-                <div class="item__output">
-                  Выход:
-                  <span>Относительная ссылка</span>
-                </div>
-              </div>
-            </div>
-          </scrollbar>
-        </div>
+        <ModuleList :moduleList="moduleList"/>
         <button>Отправить</button>
       </div>
     </scrollbar>
@@ -86,18 +37,25 @@
 import { mapGetters } from 'vuex'
 import TInput from "@/components/forms/TInput.vue";
 import Checkbox from "@/components/forms/Checkbox";
+import ModuleList from "./ModuleList";
 export default {
   name: "Settings",
   components: {
     TInput,
-    Checkbox
+    Checkbox,
+    ModuleList
   },
   data: () => ({}),
   computed: {
   ...mapGetters({
     height: "settings/height",
+    moduleList: "deploy/getModuleList"
   }),
   },
+  mounted() {
+    // console.log(this.moduleList)
+  },
+
   methods: {
     click() {
       console.log();
@@ -164,62 +122,6 @@ button {
   span{
     position: relative;
     margin-left: 201px;
-  }
-}
-.load__answer{
-  height: 464px;
-  padding: 16px;
-  margin-top: 20px;
-  background: #242F3D;
-  border: 1px solid #6C7883;
-  box-sizing: border-box;
-  border-radius: 4px;
-}
-.terra-url{
-  color: #65B9F4;
-  font-size: 14px;
-  line-height: 21px;
-  font-style: italic;
-  padding-bottom: 20px;
-}
-.answer-url{
-  font-size: 14px;
-  line-height: 21px;
-  span{
-    padding-top: 20px;
-    display: block;
-    font-size: 14px;
-    line-height: 21px;
-    font-style: italic;
-    color: #65B9F4;
-  }
-}
-.module-list{
-  padding-top: 40px;
-}
-.module-list__title{
-  font-size: 16px;
-  line-height: 21px;
-  font-weight: 700;
-  padding-bottom: 10px;
-}
-.module-list__item{
-  padding: 10px 0;
-}
-.item__title{
-  font-size: 14px;
-  line-height: 21px;
-  font-weight: 700;
-}
-.item__url{
-  color: #65B9F4;
-  font-size: 14px;
-  line-height: 21px;
-  font-style: italic;
-}
-.item__input, .item__output{
-  span{
-    display: block;
   }
 }
 
