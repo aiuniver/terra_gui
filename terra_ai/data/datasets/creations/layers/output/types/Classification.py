6593a13c7530ace182aa7c06942edc7bf5e858f0
BL146U <bl146u@gmail.com>
2021-08-24 21:51:20 +0300

    -

--- terra_ai/data/datasets/creations/layers/output/types/Classification.py
+++ terra_ai/data/datasets/creations/layers/output/types/Classification.py
@@ -1,15 +1,14 @@
+from pathlib import Path
 from typing import Optional, List
 from pydantic import validator
 
+from ...extra import SourcesPathsData
 from .....extra import LayerTypeProcessingClassificationChoice
-from ......mixins import BaseMixinData
-from ......types import confilepath
 
 
-class ParametersData(BaseMixinData):
-    sources_paths: List[confilepath(ext="csv")]
+class ParametersData(SourcesPathsData):
     one_hot_encoding: bool = True
-    type_processing: LayerTypeProcessingClassificationChoice
+    type_processing: Optional[LayerTypeProcessingClassificationChoice]
     ranges: Optional[str]
 
     cols_names: Optional[List[str]]
@@ -20,4 +19,12 @@ class ParametersData(BaseMixinData):
     ) -> LayerTypeProcessingClassificationChoice:
         if value == LayerTypeProcessingClassificationChoice.ranges:
             cls.__fields__["ranges"].required = True
+        return value
+
+    @validator("sources_paths")
+    def _validate_sources_paths(cls, value: List[Path]) -> List[Path]:
+        if not len(value):
+            return value
+        if value[0].is_file():
+            cls.__fields__["type_processing"].required = True
         return value
