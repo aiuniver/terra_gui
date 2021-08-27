5f736e0ca48bf9e51b1067a656b20bf37477e41d
Ivan <45362427+Efcolipt@users.noreply.github.com>
2021-08-27 12:56:52 +0300

    fixed correct show size dataset

--- vue/src/components/datasets/cards/CardDataset.vue
+++ vue/src/components/datasets/cards/CardDataset.vue
@@ -6,14 +6,20 @@
     <div class="dataset-card">
       <div class="card-title">{{ dataset.name }}</div>
       <div class="card-body" @click="click">
-        <div v-for="({  name }, key) of getFour" :key="`tag_${key}`" class="card-tag">
+        <div v-for="({ name }, key) of getFour" :key="`tag_${key}`" class="card-tag">
           {{ name }}
         </div>
       </div>
 
       <div :class="'card-extra ' + (dataset.size ? 'is-custom' : '')">
         <div class="wrapper">
-          <span>{{ dataset.size ? dataset.size.short.toFixed(2) + " " + dataset.size.unit : 'Предустановленный' }}</span>
+          <span>
+            {{
+              dataset.size && isFinite(dataset.size) && dataset.size === parseInt(dataset.size, 10)
+                ? dataset.size.short.toFixed(2) + ' ' + dataset.size.unit
+                : 'Предустановленный'
+            }}
+          </span>
         </div>
         <div class="remove"></div>
       </div>
@@ -37,22 +43,22 @@ export default {
     },
   },
   data: () => ({
-    index: 0
+    index: 0,
   }),
   computed: {
-    getFour () {
-      const tags = this.dataset?.tags || []
-      return tags.filter((item, i) => i >= this.index && i < this.index + 4)
-    }
+    getFour() {
+      const tags = this.dataset?.tags || [];
+      return tags.filter((item, i) => i >= this.index && i < this.index + 4);
+    },
   },
   methods: {
     click() {
       if (this.dataset.tags.length > this.index + 4) {
-        this.index = this.index + 4       
+        this.index = this.index + 4;
       } else {
-        this.index = 0
+        this.index = 0;
       }
-    }
-  }
+    },
+  },
 };
 </script>
