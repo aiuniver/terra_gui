3d613ec004c1d5d222ed1cf62e2c95044d2cef6d
bondrogeen <bondrogeen@gmail.com>
2021-08-30 11:11:40 +0300

    fix copyModale

--- vue/src/components/global/modals/CopyModal.vue
+++ vue/src/components/global/modals/CopyModal.vue
@@ -1,41 +1,44 @@
 <template>
-    <at-modal v-model="dialog" width="400">
-      <div slot="header" style="text-align: center">
-        <span>{{ title }}</span>
+  <at-modal v-model="dialog" width="400">
+    <div slot="header" style="text-align: center">
+      <span>{{ title }}</span>
+    </div>
+    <div class="t-pre">
+      <scrollbar>
+        <p class="message"><slot></slot></p>
+      </scrollbar>
+    </div>
+    <div slot="footer">
+      <div class="copy-buffer">
+        <i :class="['t-icon', 'icon-clipboard']" :title="'copy'" @click="Copy"></i>
+        <p v-if="copy" class="success">Код скопирован в буфер обмена</p>
       </div>
-      <div class="t-pre">
-        <scrollbar>
-          <p class="message"><slot></slot></p>
-        </scrollbar>
-      </div>
-      <div slot="footer">
-        <div class="copy-buffer">
-          <i :class="['t-icon', 'icon-deploy-copy']" :title="'copy'" @click="Copy"></i>
-          <p>Скопировать в буфер обмена</p>
-        </div>
-      </div>
-    </at-modal>
+    </div>
+  </at-modal>
 </template>
 
 <script>
 export default {
-  name: "CopyModal",
+  name: 'CopyModal',
   props: {
     title: {
       type: String,
-      default: "Title"
+      default: 'Title',
     },
-    windowShow: Boolean
+    value: Boolean,
   },
-  methods:{
-    Copy(){
-      let text = this.$el.querySelector(".message").innerHTML;
-      var textArea = document.createElement("textarea");
+  data: () => ({
+    copy: false,
+  }),
+  methods: {
+    Copy() {
+      let text = this.$el.querySelector('.message').innerHTML;
+      var textArea = document.createElement('textarea');
       textArea.value = text;
 
-      textArea.style.top = "0";
-      textArea.style.left = "0";
-      textArea.style.position = "fixed";
+      textArea.style.top = '0';
+      textArea.style.left = '0';
+      textArea.style.position = 'fixed';
 
       document.body.appendChild(textArea);
       textArea.focus();
@@ -43,24 +46,28 @@ export default {
 
       try {
         document.execCommand('copy');
+        this.copy = true
       } catch (err) {
         console.error('Fallback: Oops, unable to copy', err);
       }
       document.body.removeChild(textArea);
-      alert("Скопировано в буфер обмена");
-    }
+      // alert("Скопировано в буфер обмена");
+    },
   },
   computed: {
     dialog: {
-      set(value){
-        this.$emit('closeModal', value);
+      set(value) {
+        this.$emit('input', value);
+        if (!value) {
+          this.copy = value
+        }
       },
-      get(){
-        return this.windowShow
-      }
-    }
-  }
-}
+      get() {
+        return this.value;
+      },
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
@@ -73,12 +80,13 @@ export default {
   }
 }
 
-.copy-buffer{
+.copy-buffer {
   display: flex;
   align-items: center;
-  p{
-    margin-left: 5px;
+  p {
+    margin-left: 15px;
+    font-size: 0.85rem;
+    color: #3eba31;
   }
 }
-
 </style>
