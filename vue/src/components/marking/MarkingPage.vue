942470160defd655551a24d811d0ca1eed2b77ad
bondrogeen <bondrogeen@gmail.com>
2021-09-01 13:22:57 +0300

    marking: clear marking page

--- vue/src/components/marking/MarkingPage.vue
+++ vue/src/components/marking/MarkingPage.vue
@@ -1,15 +1,19 @@
 <template>
+
   <div class="board">
-    <div class="wrapper">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore cum, tempore ex excepturi quis magnam animi
-        odit minima asperiores dicta natus quibusdam corporis sed ipsa nisi possimus eum itaque dolorem!
-      </p>
+    <div class="board__inner">
+      <div :class="['board__files', { toggle: !toggle }]">
+        <BlockFiles @toggle="change" />
+      </div>
+      <div class="board__main">
+        
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+
 import { mapGetters } from 'vuex';
 
 export default {
@@ -26,8 +30,38 @@ export default {
 
 <style lang="scss" scoped>
 .board {
-  flex-shrink: 1;
+  flex-shrink: 0;
   width: 100%;
-  background-color: #17212b;
+  // padding-left: 41px;
+  flex: auto;
+  display: -webkit-flex;
+  display: flex;
+  background-color: #0e1621;
+  border-top: #0e1621 solid 1px;
+  &__inner {
+    width: 100%;
+    display: -webkit-flex;
+    display: flex;
+    position: relative;
+    background-color: #17212b;
+  }
+  &__files {
+    flex: 0 0 190px;
+    display: -webkit-flex;
+    display: flex;
+    border-right: #0e1621 solid 1px;
+    &.toggle {
+      flex: 0 0 40px;
+    }
+  }
+  &__main {
+    flex: 1 1;
+    display: -webkit-flex;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    overflow: hidden;
+    
+  }
 }
 </style>
---
+++ vue/src/components/marking/params/components/card/CardMarking.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="t-card-marking">
+    <div class="t-card-marking__left"></div>
+    <div class="t-card-marking__center"></div>
+    <div class="t-card-marking__right"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 't-card-marking',
+  props: {
+    label: String,
+    type: String,
+  },
+  computed: {
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.t-card-marking {
+  
+}
+</style>
