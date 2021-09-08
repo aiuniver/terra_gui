317bb2a3fa1639d229873e54ec99fcb71c19105e
Denis Yurchuk <667766yden@gmail.com>
2021-09-08 18:17:46 +0300

    change validator recept output shape

--- terra_ai/modeling/validator.py
+++ terra_ai/modeling/validator.py
@@ -17,7 +17,7 @@ from terra_ai.data.modeling.extra import LayerGroupChoice, LayerTypeChoice
 from terra_ai.data.modeling.layer import LayerData
 from terra_ai.data.modeling.model import ModelDetailsData
 
-__version__ = 0.052
+__version__ = 0.053
 
 from terra_ai.data.modeling.layers.extra import (
     ModuleTypeChoice,
