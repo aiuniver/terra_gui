9a59b04a6c61865b703daedbd07408821cb80e97
bondrogeen <bondrogeen@gmail.com>
2021-09-11 22:33:08 +0300

    modeling: minor fix

--- vue/src/store/modules/modeling.js
+++ vue/src/store/modules/modeling.js
@@ -109,9 +109,10 @@ export default {
     async info({ dispatch }, value) {
       return await dispatch('axios', { url: '/modeling/info/', data: value }, { root: true });
     },
-    async load({ dispatch }, value) {
+    async load({ commit, dispatch }, value) {
       const { data: model } = await dispatch('axios', { url: '/modeling/load/', data: value }, { root: true });
       if (model) {
+        commit('SET_ERRORS_BLOCKS', {});
         await dispatch('projects/get', {}, { root: true });
       }
       return model;
