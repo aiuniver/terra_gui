ed2f9437f1a842bcf4b473b389f299239eb9a3de
aleksandr_kononov <kononov52rus@gmail.com>
2021-09-15 14:12:04 +0300

    integration to array_create()

--- terra_ai/datasets/arrays_create.py
+++ terra_ai/datasets/arrays_create.py
@@ -68,14 +68,13 @@ class CreateArray(object):
                         subdf = df.iloc[i, :]
                         subdf = subdf.dropna().values.tolist()
                         for j in range(0, len(subdf), step_len):
-                            if len(subdf[j: j + xlen]) < xlen:
+                            if len(subdf[j: j + step_len]) < xlen:
                                 xlen_array.append(subdf[-xlen:])
                             else:
                                 xlen_array.append(subdf[j: j + xlen])
-                    tmp_dict = {}
-                    for i in range(xlen):
-                        tmp_dict.update({i: np.array(xlen_array)[:, i]})
-                    df = pd.DataFrame(tmp_dict)
+
+                if options["parameters"]["xlen_step"]:
+                    df = pd.DataFrame({"slices": xlen_array})
                 instructions["parameters"]["scaler"] = options["parameters"]["scaler"]
             else:
                 tmp_df = pd.read_csv(options["parameters"]["sources_paths"][0],
@@ -91,37 +90,37 @@ class CreateArray(object):
                 df = df.loc[:, x_cols]
         instructions["instructions"] = df.to_dict()
 
-        if options["parameters"]["Categorical_cols"]:
-            tmp_lst = options["parameters"]["Categorical_cols"]
-            instructions["parameters"]["Categorical_cols"] = {}
-            instructions["parameters"]["Categorical_cols"]["lst_cols"] = tmp_lst
-            for i in instructions["parameters"]["Categorical_cols"]["lst_cols"]:
-                instructions["parameters"]["Categorical_cols"][f"col_{i}"] = list(set(df.iloc[:, i]))
+        if options["parameters"]["Categorical"]:
+            tmp_lst = options["parameters"]["Categorical"]
+            instructions["parameters"]["Categorical"] = {}
+            instructions["parameters"]["Categorical"]["lst_cols"] = tmp_lst
+            for i in instructions["parameters"]["Categorical"]["lst_cols"]:
+                instructions["parameters"]["Categorical"][f"col_{i}"] = list(set(df.iloc[:, i]))
 
-        if options["parameters"]["Categorical_ranges_cols"]:
-            tmp_lst = options["parameters"]["Categorical_ranges_cols"]
-            instructions["parameters"]["Categorical_ranges_cols"] = {}
-            instructions["parameters"]["Categorical_ranges_cols"]["lst_cols"] = tmp_lst
+        if options["parameters"]["Categorical_ranges"]:
+            tmp_lst = options["parameters"]["Categorical_ranges"]
+            instructions["parameters"]["Categorical_ranges"] = {}
+            instructions["parameters"]["Categorical_ranges"]["lst_cols"] = tmp_lst
             for i in range(len(tmp_lst)):
                 if len(list(options["parameters"]["cat_cols"].values())[i].split(" ")) == 1:
                     border = max(df.iloc[:, tmp_lst[i]]) / int(list(options["parameters"]["cat_cols"].values())[i])
-                    instructions["parameters"]["Categorical_ranges_cols"][f"col_{tmp_lst[i]}"] = np.linspace(
+                    instructions["parameters"]["Categorical_ranges"][f"col_{tmp_lst[i]}"] = np.linspace(
                         border, max(df.iloc[:, tmp_lst[i]]), int(list(options["parameters"]["cat_cols"].values())[i])).tolist()
                 else:
-                    instructions["parameters"]["Categorical_ranges_cols"][f"col_{tmp_lst[i]}"] = \
+                    instructions["parameters"]["Categorical_ranges"][f"col_{tmp_lst[i]}"] = \
                         list(options["parameters"]["cat_cols"].values())[i].split(" ")
 
-        if options["parameters"]["one_hot_encoding_cols"]:
-            tmp_lst = options["parameters"]["one_hot_encoding_cols"]
-            instructions["parameters"]["one_hot_encoding_cols"] = {}
-            instructions["parameters"]["one_hot_encoding_cols"]["lst_cols"] = tmp_lst
-            for i in instructions["parameters"]["one_hot_encoding_cols"]["lst_cols"]:
-                if options["parameters"]["Categorical_ranges_cols"] and (
-                        i in instructions["parameters"]["Categorical_ranges_cols"]["lst_cols"]):
-                    instructions["parameters"]["one_hot_encoding_cols"][f"col_{i}"] = len(
-                        instructions["parameters"]["Categorical_ranges_cols"][f"col_{i}"])
+        if options["parameters"]["one_hot_encoding"]:
+            tmp_lst = options["parameters"]["one_hot_encoding"]
+            instructions["parameters"]["one_hot_encoding"] = {}
+            instructions["parameters"]["one_hot_encoding"]["lst_cols"] = tmp_lst
+            for i in instructions["parameters"]["one_hot_encoding"]["lst_cols"]:
+                if options["parameters"]["Categorical_ranges"] and (
+                        i in instructions["parameters"]["Categorical_ranges"]["lst_cols"]):
+                    instructions["parameters"]["one_hot_encoding"][f"col_{i}"] = len(
+                        instructions["parameters"]["Categorical_ranges"][f"col_{i}"])
                 else:
-                    instructions["parameters"]["one_hot_encoding_cols"][f"col_{i}"] = len(
+                    instructions["parameters"]["one_hot_encoding"][f"col_{i}"] = len(
                         set(df.iloc[:, i]))
         return instructions
 
@@ -314,14 +313,13 @@ class CreateArray(object):
                         y_class.append(classes_names[i])
             paths_list = y_class
 
-        elif os.path.isfile(options['parameters']['sources_paths'][0]) and \
+        if os.path.isfile(options['parameters']['sources_paths'][0]) and \
                 options['parameters']['sources_paths'][0].endswith('.csv'):
             file_name = options['parameters']['sources_paths'][0]
-            data = pd.read_csv(file_name, usecols=options['parameters']['cols_names'],
-                                     sep=options["parameters"]["separator"])
+            data = pd.read_csv(file_name, usecols=options['parameters']['cols_names'])
             data.sort_values(by=data.columns[0], ignore_index=True, inplace=True)
             column = data.iloc[:, 0].to_list()
-
+            type_processing = options['parameters']['type_processing']
             if type_processing == "categorical":
                 classes_names = []
                 for elem in column:
@@ -462,6 +460,38 @@ class CreateArray(object):
 
         return instructions
 
+    @staticmethod
+    def instructions_timeseries(_, **options: dict) -> dict:
+
+        instructions = {"instructions": {}, "parameters": options["parameters"]}
+        instructions["parameters"]["put"] = options["id"]
+        if options["parameters"]["transpose"]:
+            tmp_df_ts = pd.read_csv(options["parameters"]["sources_paths"][0], sep=options["parameters"]["separator"]).T
+            tmp_df_ts.columns = tmp_df_ts.iloc[0]
+            tmp_df_ts.drop(tmp_df_ts.index[[0]], inplace=True)
+            tmp_df_ts.index = range(0, len(tmp_df_ts))
+            for i in instructions["parameters"]["cols_names"]:
+                tmp_df_ts = tmp_df_ts.astype({i: np.float}, errors="ignore")
+            y_subdf = tmp_df_ts.loc[:, instructions["parameters"]["cols_names"]]
+        else:
+            y_subdf = pd.read_csv(
+                options["parameters"]["sources_paths"][0], sep=options["parameters"]["separator"],
+                usecols=instructions["parameters"]["cols_names"])
+
+        if options["parameters"]['trend']:
+            instructions['parameters']['classes_names'] = ["Не изменился", "Вверх", "Вниз"]
+            instructions['parameters']['num_classes'] = 3
+        instructions["instructions"] = y_subdf.to_dict()
+        return instructions
+
+
+    @staticmethod
+    def cut_dataframe(paths_list: dict, tmp_folder=None, dataset_folder=None, **options: dict):
+
+        instructions = {'instructions': paths_list,
+                        'parameters': options}
+
+        return instructions
 
     @staticmethod
     def instructions_timeseries(_, **options: dict) -> dict:
@@ -697,6 +727,22 @@ class CreateArray(object):
 
         return instructions
 
+    @staticmethod
+    def cut_timeseries(paths_list: dict, tmp_folder=None, dataset_folder=None, **options: dict):
+
+        instructions = {'instructions': paths_list,
+                        'parameters': options}
+
+        return instructions
+
+
+    @staticmethod
+    def create_dataframe(row, **options) -> dict:
+
+        instructions = {'instructions': row,
+                        'parameters': options}
+
+        return instructions
 
     @staticmethod
     def cut_timeseries(paths_list: dict, tmp_folder=None, dataset_folder=None, **options: dict):
@@ -878,6 +924,76 @@ class CreateArray(object):
 
         return instructions
 
+    @staticmethod
+    def create_timeseries(row, **options) -> dict:
+
+        instructions = {'instructions': row,
+                        'parameters': options}
+
+        return instructions
+
+
+    @staticmethod
+    def preprocess_dataframe(row: np.ndarray, **options) -> np.ndarray:
+        length = options['length'] if 'timeseries' in options.keys() else 1
+        if length == 1:
+            row = row[0] if options['xlen_step'] else [row]
+        if options['scaler'] != 'no_scaler':
+            row = np.array(row)
+            orig_shape = row.shape
+            array = options['object_scaler'].transform(row.reshape(-1, 1))
+            array = array.reshape(orig_shape)
+            return array
+
+        if options['MinMaxScaler']:
+            for j in range(length):
+                for i in options['MinMaxScaler']:
+                    row[j][i] = options['object_scaler'][f'col_{i}'].transform(
+                        np.array(row[j][i]).reshape(-1, 1)).tolist()[0][0]
+
+        if options['StandardScaler']:
+            for j in range(length):
+                for i in options['StandardScaler']:
+                    row[j][i] = options['object_scaler'][f'col_{i}'].transform(
+                        np.array(row[j][i]).reshape(-1, 1)).tolist()[0][0]
+
+        if options['Categorical']:
+            for j in range(length):
+                for i in options['Categorical']['lst_cols']:
+                    row[j][i] = list(options['Categorical'][f'col_{i}']).index(row[j][i])
+
+        if options['Categorical_ranges']:
+            for j in range(length):
+                for i in options['Categorical_ranges']['lst_cols']:
+                    for k in range(len(options['Categorical_ranges'][f'col_{i}'])):
+                        if row[j][i] <= int(options['Categorical_ranges'][f'col_{i}'][k]):
+                            row[j][i] = k
+                            break
+
+        if options['one_hot_encoding']:
+            for j in range(length):
+                for i in options['one_hot_encoding']['lst_cols']:
+                    row[j][i] = utils.to_categorical(row[j][i], options['one_hot_encoding'][f'col_{i}'],
+                                                     dtype='uint8').tolist()
+
+        if type(row) != list:
+            row = row.tolist()
+
+        array = []
+        for i in row:
+            tmp = []
+            for j in i:
+                if type(j) == list:
+                    if type(j[0]) == list:
+                        tmp.extend(j[0])
+                    else:
+                        tmp.extend(j)
+                else:
+                    tmp.append(j)
+            array.append(tmp)
+
+        array = np.array(array)
+        return array
 
     @staticmethod
     def create_timeseries(row, **options) -> dict:
