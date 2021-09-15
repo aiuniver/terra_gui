b8d306732a1cda91456289a4b3125015558055d5
Denis Yurchuk <667766yden@gmail.com>
2021-09-15 21:22:34 +0300

    -

--- terra_ai/training/customcallback.py
+++ terra_ai/training/customcallback.py
@@ -23,7 +23,7 @@ from terra_ai.data.training.extra import TaskChoice
 from terra_ai.datasets.preparing import PrepareDataset
 from terra_ai.utils import camelize, decamelize
 
-__version__ = 0.055
+__version__ = 0.056
 
 
 def sort_dict(dict_to_sort: dict, mode='by_name'):
