8c5dc3694cb590bb277b6a1588dc76ded670a17b
SvyatS <prisenko2002@gmail.com>
2021-09-17 16:30:05 +0700

    predict on training page

--- vue/src/components/training/main/prediction/TextTableTest.vue
+++ vue/src/components/training/main/prediction/TextTableTest.vue
@@ -3,111 +3,73 @@
     <div class="table__columns">
       <div class="table__column">
         <div class="table__item title size-1">Слой</div>
-        <div class="table__item">1</div>
-        <div class="table__item">2</div>
-        <div class="table__item">3</div>
-        <div class="table__item">4</div>
+        <div class="table__item" v-for="index in Object.keys(predict.initial_value[Object.keys(predict.initial_value)[0]]).length" :key="index">{{ index }}</div>
       </div>
       <div class="table__column">
-        <div class="table__item">
-          <div class="table__column">
+          <div class="table__column" v-if="predict.initial_value">
             <div class="table__item title size-2">
               Исходные данные
             </div>
             <div class="table__row">
-              <div class="table__column">
-                <div class="table__item title size-2">Вход 1</div>
-                <div class="table__item">FROG</div>
-                <div class="table__item">TRUCK</div>
-                <div class="table__item">SHIP</div>
-                <div class="table__item">FROG</div>
+              <div class="table__column" v-for="(value, input_name) in predict.initial_value" :key="input_name">
+                <div class="table__item title size-2">{{ input_name }}</div>
+                <div class="table__item"  v-for="(input_val, key) in value" :key="key">
+                   <img v-if="input_val.type == 'image'" :src="require('@/../public/imgs/'+input_val.data)" :alt="'img'" width="270">
+                    <p v-if="input_val.type == 'str' || input_val.type == 'number'">{{ input_val.data }}</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
       </div>
       <div class="table__column">
-        <div class="table__item">
-          <div class="table__column">
+          <div class="table__column" v-if="predict.true_value">
             <div class="table__item title size-2">
               Истинное значение
             </div>
             <div class="table__row">
-              <div class="table__column">
-                <div class="table__item title size-2">Выход 2</div>
-                <div class="table__item">FROG</div>
-                <div class="table__item">TRUCK</div>
-                <div class="table__item">SHIP</div>
-                <div class="table__item">FROG</div>
+              <div class="table__column" v-for="(value, input_name) in predict.true_value" :key="input_name">
+                <div class="table__item title size-2">{{ input_name }}</div>
+                <div class="table__item" v-for="(input_val, key) in value" :key="key">
+                   <img v-if="input_val.type == 'image'" :src="require('@/../public/imgs/'+input_val.data)" :alt="'img'" width="270">
+                    <p v-if="input_val.type == 'str' || input_val.type == 'number'">{{ input_val.data }}</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
       </div>
       <div class="table__column">
-        <div class="table__item">
-          <div class="table__column">
-            <div class="table__column title size-2">
+          <div class="table__column" v-if="predict.predict_value">
+            <div class="table__item title size-2">
               Предсказание
             </div>
             <div class="table__row">
-              <div class="table__column">
-                <div class="table__item title size-2">Выход 3</div>
-                <div class="table__item">FROG</div>
-                <div class="table__item">TRUCK</div>
-                <div class="table__item">SHIP</div>
-                <div class="table__item">FROG</div>
+              <div class="table__column" v-for="(value, input_name) in predict.predict_value" :key="input_name">
+                <div class="table__item title size-2">{{ input_name }}</div>
+                <div class="table__item" v-for="(input_val, key) in value" :key="key">
+                  <img v-if="input_val.type == 'image'" :src="require('@/../public/imgs/'+input_val.data)" :alt="'img'" width="270">
+                    <p v-if="input_val.type == 'str' || input_val.type == 'number'">{{ input_val.data }}</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
       </div>
       <div class="table__column">
-        <div class="table__item">
-          <div class="table__column">
+          <div class="table__column" v-if="predict.statistic_values">
             <div class="table__item title size-2">
               Статистика примеров
             </div>
             <div class="table__row">
-              <div class="table__column">
-                <div class="table__item title size-2">airplane</div>
-                <div class="table__item">0.9%</div>
-                <div class="table__item">0.9%</div>
-                <div class="table__item">0.9%</div>
-                <div class="table__item">0.9%</div>
-              </div>
-              <div class="table__column">
-                <div class="table__item title size-2">frog</div>
-                <div class="table__item">0.9%</div>
-                <div class="table__item">0.9%</div>
-                <div class="table__item">0.9%</div>
-                <div class="table__item">0.9%</div>
-              </div>
-              <div class="table__column">
-                <div class="table__item title size-2">truck</div>
-                <div class="table__item">0.9%</div>
-                <div class="table__item">0.9%</div>
-                <div class="table__item">0.9%</div>
-                <div class="table__item">0.9%</div>
-              </div>
-              <div class="table__column">
-                <div class="table__item title size-2">ship</div>
-                <div class="table__item">0.9%</div>
-                <div class="table__item">0.9%</div>
-                <div class="table__item">0.9%</div>
-                <div class="table__item">0.9%</div>
-              </div>
-              <div class="table__column">
-                <div class="table__item title size-2">horse</div>
-                <div class="table__item">0.9%</div>
-                <div class="table__item">0.9%</div>
-                <div class="table__item">0.9%</div>
-                <div class="table__item">0.9%</div>
+              <div class="table__column" v-for="(value, input_name) in predict.statistic_values" :key="input_name">
+                <div class="table__item title size-2">{{ input_name }}</div>
+                <div class="table__item" v-for="(input_val, key) in value" :key="key">
+                  <img v-if="input_val.type == 'image'" :src="require('@/../public/imgs/'+input_val.data)" :alt="'img'" width="270">
+                    <p v-if="input_val.type == 'str' || input_val.type == 'number'">{{ input_val.data }}</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+
       </div>
     </div>
   </div>
@@ -146,6 +108,7 @@ $border: #0e1621;
   &__column{
     display: flex;
     flex-direction: column;
+    width: fill-available;
     height: 100%;
   }
   &__item{
@@ -153,10 +116,14 @@ $border: #0e1621;
     align-items: center;
     justify-content: center;
     width: 100%;
+    border-right: 1px solid $border;
+    border-top: 1px solid $border;
   }
   .title{
     background: $bgGray;
     min-width: 70px;
+    border-top: none;
+    border-bottom: 1px solid $border;
   }
 }
 
