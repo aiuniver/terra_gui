cee1da814a224eb8f4a651ab4827aab4715e14f7
Danil Popov <zabastx@gmail.com>
2021-09-19 18:02:15 +0300

    show graphs condition

--- vue/src/components/training/main/balance/index.vue
+++ vue/src/components/training/main/balance/index.vue
@@ -4,23 +4,21 @@
       <div class="t-balance__wrapper">
         <div class="t-balance__checks">
           <t-field inline :label="'Показать тренировочную выборку'">
-            <t-checkbox-new />
+            <t-checkbox-new v-model="graph1" />
           </t-field>
           <t-field inline :label="'Показать проверочную выборку'">
-            <t-checkbox-new small />
+            <t-checkbox-new v-model="graph2" small />
           </t-field>
         </div>
         <t-field inline :label="'Сортировать'">
           <t-select-new small :list="[]" />
         </t-field>
-        <t-button class="t-balance__btn">Показать</t-button>
+        <t-button class="t-balance__btn" @click="handleClick">Показать</t-button>
       </div>
     </div>
     <div class="t-balance__graphs">
-      <template v-for="(id, index) of dataDalance">
-        <template v-for="(item, i) of id">
-          <Graph :key="'graph_' + index + '/' + i" v-bind="item" />
-        </template>
+      <template v-for="(item, i) of showGraphs">
+        <Graph :key="'graph_' + i + '/' + i" v-bind="item" />
       </template>
     </div>
   </div>
@@ -34,11 +32,24 @@ export default {
   components: {
     Graph,
   },
+  data: () => ({
+    graph1: false,
+    graph2: false
+  }),
   computed: {
     dataDalance() {
       return this.$store.getters['trainings/getTrainData']('data_balance') || [];
     },
+    showGraphs() {
+      return this.dataDalance[2].filter(item => this[`graph${item.id}`])
+    }
   },
+  methods: {
+    async handleClick() {
+      const res = await this.$store.dispatch('trainings/interactive', {})
+      console.log(`response`, res);
+    }
+  }
 };
 </script>
 
