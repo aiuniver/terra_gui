5e9ac991aedf46a5b54c4a816a40b7dab92461c4
Ivan <45362427+Efcolipt@users.noreply.github.com>
2021-09-20 23:29:15 +0300

    training: test text

--- vue/src/components/training/main/prediction/TextTableTest.vue
+++ vue/src/components/training/main/prediction/TextTableTest.vue
@@ -3,77 +3,76 @@
     <div class="table__columns">
       <div class="table__column">
         <div class="table__item title size-1 layer-title">Слой</div>
-        <div
-          class="table__item"
-          v-for="index in Object.keys(predict.initial_value[Object.keys(predict.initial_value)[0]]).length"
-          :key="index"
-        >
+        <div class="table__item" v-for="(value, index) in counterSloy" :key="index">
           {{ index }}
         </div>
       </div>
-      <div class="table__column">
-        <div class="table__column" v-if="predict.initial_value">
+
+      <div class="table__column" v-if="predict.initial_data.length">
+        <div class="table__column">
           <div class="table__item title size-2">Исходные данные</div>
           <div class="table__row">
-            <div class="table__column" v-for="(value, input_name) in predict.initial_value" :key="input_name">
+            <div class="table__column" v-for="(value, input_name) in predict.initial_data" :key="input_name">
               <div class="table__item title size-2">{{ input_name }}</div>
-              <div class="table__item" v-for="(input_val, key) in value" :key="key">
-                <TableImage v-if="input_val.type == 'image'" :image="input_val" />
-                <TableText v-if="input_val.type === 'str' || input_val.type === 'number'" :data="input_val" />
-                <TableColorText v-if="input_val.type === 'Text'" :data="input_val" />
-                <Embed v-if="input_val.type === 'video'" :src="input_val.data"></Embed>
-                <TableAudio v-if="input_val.type === 'audio'" :url="input_val.data" />
+              <div class="table__item">
+                <TableImage v-if="value.type == 'image'" :image="value" />
+                <TableText v-if="value.type === 'str' || value.type === 'number'" :data="value" />
+                <TableTag v-if="value.type === 'Text'" :data="value" />
+
+                <Embed v-if="value.type === 'video'" :src="value.data"></Embed>
+                <TableAudio v-if="value.type === 'audio'" :url="value.data" />
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="table__column">
-        <div class="table__column" v-if="predict.true_value">
+
+      <div class="table__column" v-if="predict.true_value">
+        <div class="table__column">
           <div class="table__item title size-2">Истинное значение</div>
           <div class="table__row">
             <div class="table__column" v-for="(value, input_name) in predict.true_value" :key="input_name">
               <div class="table__item title size-2">{{ input_name }}</div>
-              <div class="table__item" v-for="(input_val, key) in value" :key="key">
-                <TableImage v-if="input_val.type == 'image'" :image="input_val" />
-                <TableText v-if="input_val.type === 'str' || input_val.type === 'number'" :data="input_val" />
-                <TableColorText v-if="input_val.type === 'Text'" :data="input_val" />
+              <div class="table__item">
+                <TableImage v-if="value.type == 'image'" :image="value" />
+                <TableText v-if="value.type === 'str' || value.type === 'number'" :data="value" />
+                <TableTag v-if="value.type === 'Text'" :data="value" />
 
-                <Embed v-if="input_val.type === 'video'" :src="input_val.data"></Embed>
-                <TableAudio v-if="input_val.type === 'audio'" :url="input_val.data" />
+                <Embed v-if="value.type === 'video'" :src="value.data"></Embed>
+                <TableAudio v-if="value.type === 'audio'" :url="value.data" />
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="table__column">
-        <div class="table__column" v-if="predict.predict_value">
+
+      <div class="table__column" v-if="predict.predict_value">
+        <div class="table__column">
           <div class="table__item title size-2">Предсказание</div>
           <div class="table__row">
             <div class="table__column" v-for="(value, input_name) in predict.predict_value" :key="input_name">
               <div class="table__item title size-2">{{ input_name }}</div>
-              <div class="table__item" v-for="(input_val, key) in value" :key="key">
-                <TableImage v-if="input_val.type == 'image'" :image="input_val" />
-                <TableText v-if="input_val.type === 'str' || input_val.type === 'number'" :data="input_val" />
-                <TableColorText v-if="input_val.type === 'Text'" :data="input_val" />
+              <div class="table__item">
+                <TableImage v-if="value.type == 'image'" :image="value" />
+                <TableText v-if="value.type === 'str' || value.type === 'number'" :data="value" />
+                <TableTag v-if="value.type === 'Text'" :data="value" />
 
-                <Embed v-if="input_val.type === 'video'" :src="input_val.data"></Embed>
-                <TableAudio v-if="input_val.type === 'audio'" :url="input_val.data" />
+                <Embed v-if="value.type === 'video'" :src="value.data"></Embed>
+                <TableAudio v-if="value.type === 'audio'" :url="value.data" />
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="table__column">
-        <div class="table__column" v-if="predict.statistic_values">
+
+      <div class="table__column" v-if="predict.statistic_values">
+        <div class="table__column">
           <div class="table__item title size-2">Статистика примеров</div>
           <div class="table__row">
             <div class="table__column" v-for="(value, input_name) in predict.statistic_values" :key="input_name">
               <div class="table__item title size-2">{{ input_name }}</div>
-              <div class="table__item" v-for="(input_val, key) in value" :key="key">
-                <TableImage v-if="input_val.type == 'image'" :image="input_val" />
-                <TableText v-if="input_val.type === 'str' || input_val.type === 'number'" :data="input_val" />
-                <TableColorText v-if="input_val.type === 'Text'" :data="input_val" />
+              <div class="table__item" v-for="(input_val, key) in value.data" :key="key">
+                <TableStatisticText :data="input_val" :value="key" />
               </div>
             </div>
           </div>
@@ -86,16 +85,18 @@
 <script>
 import TableImage from '@/components/training/main/prediction/components/TableImage.vue';
 import TableText from '@/components/training/main/prediction/components/TableText.vue';
-import TableColorText from '@/components/training/main/prediction/components/TableColorText.vue';
+import TableStatisticText from '@/components/training/main/prediction/components/TableStatisticText.vue';
+import TableTag from '@/components/training/main/prediction/components/TableTag.vue';
 import TableAudio from '../audio/TableAudio';
 import Embed from 'v-video-embed/src/embed';
 export default {
   name: 'TextTableTest',
   components: {
     TableImage,
-    TableText,
+    TableStatisticText,
     TableAudio,
-    TableColorText,
+    TableText,
+    TableTag,
     Embed,
   },
   props: {
@@ -108,6 +109,13 @@ export default {
   data: () => ({}),
   created() {
     console.log('Predict', this.predict);
+  },
+  computed: {
+    counterSloy() {
+      return Object.keys(this.predict.initial_data).length == 0
+        ? 1
+        : Object.keys(this.predict.initial_value[Object.keys(this.predict.initial_value)[0]]).length;
+    },
   },
 };
 </script>
---
+++ vue/src/components/training/main/prediction/components/TableStatisticText.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="table-prediction">
+    <p :class="['table-prediction__text']">
+      {{ value }}
+    </p>
+    <div :class="['table-prediction__color', { [`table-prediction__color--marked-${color}`]: color }]"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TableStatisticText',
+  props: {
+    data: {
+      type: [Object, Array],
+      default: () => {},
+    },
+    value: String,
+  },
+  computed: {
+    color() {
+      return this.data?.color_mark || '';
+    },
+    percent() {
+      return this.data?.value || '';
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.table-prediction {
+  display: flex;
+  &__text {
+    margin-right: 10px;
+    &--marked-success {
+      color: green;
+    }
+    &--marked-wrong {
+      color: orange;
+    }
+  }
+  &__color {
+    width: 77px;
+    height: 24px;
+    border-radius: 4px;
+    &--marked-success {
+      background: green;
+    }
+    &--marked-wrong {
+      background: orange;
+    }
+  }
+}
+</style>
