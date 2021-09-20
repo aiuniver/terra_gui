c9adbc6a968d651f4d9af9ddc362df1eae896817
bondrogeen <bondrogeen@gmail.com>
2021-09-20 23:29:38 +0300

    update build

--- vue/src/components/training/main/chars/index.vue
+++ vue/src/components/training/main/chars/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="t-charts">
     <!-- <div class="charts__title">Графики</div> -->
-    <div v-if="data.length" class="t-charts__content">
+    <div class="t-charts__content">
       <TCharTemp class="t-chart" @click.native="add" />
       <TChar
         v-for="(settings, i) of charts"
@@ -9,11 +9,11 @@
         class="t-chart"
         :key="'char1_' + i"
         :settings="settings"
-        :menus="menus"
+        :menus="allMenus"
         @event="event($event, settings)"
       />
     </div>
-    <div v-else class="t-charts__empty">Нет данных</div>
+    <div v-if="!data.length" class="t-charts__empty">Нет данных</div>
   </div>
 </template>
 
@@ -36,17 +36,17 @@ export default {
       {
         name: 'Показывать данные',
         list: [
-          { title: 'По всей модели', event: { name: 'data', data: 'loss' } },
-          { title: 'По классам', event: { name: 'data', data: 'metric' } },
+          { title: 'По всей модели', event: { name: 'data', data: 'model' } },
+          { title: 'По классам', event: { name: 'data', data: 'classes' } },
         ],
       },
-      {
-        name: 'Показывать метрики',
-        list: [
-          { title: 'Accuracy', event: { name: 'metric', data: 'Accuracy' } },
-          { title: 'Hinge', event: { name: 'metric', data: 'Hinge' } },
-        ],
-      },
+      // {
+      //   name: 'Показывать метрики',
+      //   list: [
+      //     { title: 'Accuracy', event: { name: 'metric', data: 'Accuracy' } },
+      //     { title: 'Hinge', event: { name: 'metric', data: 'Hinge' } },
+      //   ],
+      // },
     ],
   }),
   computed: {
@@ -59,20 +59,23 @@ export default {
     data() {
       return this.$store.getters['trainings/getTrainData'](this.metric) || [];
     },
+    allMenus() {
+      console.log([...this.menus, this.listChats]);
+      return [...this.menus, this.listChats];
+    },
+    listChats() {
+      const list = this.data.map(item => {
+        return { title: item.graph_name, event: { name: 'chart', data: item.id } };
+      });
+      return {
+        name: 'Выходы',
+        list,
+      };
+    },
   },
-  mounted() {
-    this.$emit('isLoad', true);
-    console.log(this.data);
-    const list = this.data.map(item => {
-      return { title: item.graph_name, event: { name: 'chart', data: item.id } };
-    });
-    this.menus.push({
-      name: 'Выходы',
-      list,
-    });
-  },
+  mounted() {},
   methods: {
-    event({ name, data }, { id }) {
+    event({ name, data }, { id, graphID }) {
       // console.log(name, data, id);
       if (data === 'remove') {
         this.remove(id);
@@ -85,23 +88,37 @@ export default {
           return item;
         });
       }
+      if (name === 'data') {
+        this.send({
+          lost_graphs: {
+            id: id,
+            output_idx: graphID,
+            show: data,
+          },
+        });
+      }
     },
-    getChart({ graphID }) {
+    getChart({ id }) {
       // console.log({ graphID });
-      return this.data.find(chart => chart.id === graphID);
+      return this.data.find(chart => chart.id === id);
     },
     add() {
       if (this.charts.length < 10) {
-        const chart = this.data.find(item => item);
-        if (chart) {
-          let maxID = Math.max(1, ...this.charts.map(o => o.id));
-          this.charts.push({ graphID: chart.id, id: maxID + 1 });
-        }
+        // const chart = this.data.find(item => item);
+        // if (chart) {
+        let maxID = Math.max(0, ...this.charts.map(o => o.id));
+        this.charts.push({ id: maxID + 1, output_idx: 2, show: 'model' });
+        this.send(this.charts);
+        // }
       }
     },
     remove(id) {
       this.charts = this.charts.filter(item => item.id !== id);
       // console.log(id);
+    },
+    async send(data) {
+      const res = await this.$store.dispatch('trainings/interactive', { lost_graphs: data });
+      console.log(`response`, res);
     },
   },
 };
