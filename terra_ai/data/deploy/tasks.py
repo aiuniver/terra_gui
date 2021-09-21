f426503eebe4424875666cc7d2def097183d03db
BL146U <bl146u@gmail.com>
2021-09-21 09:03:12 +0300

    -

--- terra_ai/data/deploy/tasks.py
+++ terra_ai/data/deploy/tasks.py
@@ -4,19 +4,13 @@ from terra_ai.data.datasets.dataset import DatasetPathsData
 
 
 class BaseCollection(BaseMixinData):
-    path: DatasetPathsData
-
     def __init__(self, dataset: DatasetData, **data):
         super().__init__(**data)
 
-    def dict(self, **kwargs):
-        kwargs.update(
-            {
-                "exclude": {"path"},
-            }
-        )
-        return super().dict(**kwargs)
-
 
 class ImageClassificationCollection(BaseCollection):
+    pass
+
+
+class ImageSegmentationCollection(BaseCollection):
     pass
