31ee991fe2db7db649129e7834ee0bd685b39ae7
BL146U <bl146u@gmail.com>
2021-09-25 15:19:03 +0300

    -

--- apps/api/training/views.py
+++ apps/api/training/views.py
@@ -91,11 +91,13 @@ class InteractiveAPIView(BaseAPIView):
         try:
             config = InteractiveData(**request.data)
             request.project.training.interactive = config
+            output_data: dict = None
             if request.project.training.state.status != StateStatusChoice.no_train:
                 training_data = agent_exchange("training_interactive", config=config)
                 if training_data:
                     request.project.training.interactive = training_data
-            return BaseResponseSuccess()
+                    output_data = training_data.native()
+            return BaseResponseSuccess(output_data)
         except ExchangeBaseException as error:
             return BaseResponseErrorGeneral(str(error))
 
