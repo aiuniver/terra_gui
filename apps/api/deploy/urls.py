8669d754db5dc10b5f335b3deed1b58a2b80087b
BL146U <bl146u@gmail.com>
2021-09-26 21:32:30 +0300

    -

--- apps/api/deploy/urls.py
+++ apps/api/deploy/urls.py
@@ -6,6 +6,7 @@ from . import views
 app_name = "deploy"
 
 urlpatterns = [
+    path("reload/", views.ReloadAPIView.as_view(), name="reload"),
     path("upload/", views.UploadAPIView.as_view(), name="upload"),
     path(
         "upload/progress/",
