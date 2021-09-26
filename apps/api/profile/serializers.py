3b59feb461deb41dc5bdfbb88cff31dbd3f79cff
aladdin <aladdin@kde.hp>
2021-09-26 21:53:25 +0300

    profile: saveserializer

--- apps/api/profile/serializers.py
+++ apps/api/profile/serializers.py
@@ -2,8 +2,5 @@ from rest_framework import serializers
 
 
 class SaveSerializer(serializers.Serializer):
-    login = serializers.CharField(required=True)
     first_name = serializers.CharField(required=True)
     last_name = serializers.CharField(required=True)
-    email = serializers.EmailField(required=True)
-    token = serializers.CharField(required=True)
