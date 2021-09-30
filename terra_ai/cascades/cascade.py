f89b7ae7a866ba55c51d8fe75fefc315b6bb2ccd
Evgeniy Sevostyanihin <57677410+BabylenMagnus@users.noreply.github.com>
2021-09-30 11:19:27 +0300

    решение багов с чтением текстов

--- terra_ai/cascades/cascade.py
+++ terra_ai/cascades/cascade.py
@@ -127,6 +127,7 @@ class CompleteCascade(Cascade):
                     out.choose_path(path)
 
         for img in self.input(input_path):
+            print(img)
             self.cascade_block(img)
 
         for i in self.output:
