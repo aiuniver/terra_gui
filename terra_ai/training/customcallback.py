f92566a95408d545fd94af7d3eade3d54ca46d46
Denis Yurchuk <667766yden@gmail.com>
2021-10-01 09:10:18 +0300

    fix numpy rgb colors

--- terra_ai/training/customcallback.py
+++ terra_ai/training/customcallback.py
@@ -28,7 +28,7 @@ from terra_ai.data.training.train import InteractiveData
 from terra_ai.datasets.preparing import PrepareDataset
 from terra_ai.utils import camelize, decamelize
 
-__version__ = 0.079
+__version__ = 0.080
 
 
 def sort_dict(dict_to_sort: dict, mode='by_name'):
