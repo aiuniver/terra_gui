d94d0d1560f276afb1f36b28068e891e597f1918
Alexey-Krapivnitsky <cronos1009@yandex.ru>
2021-10-09 14:04:28 +0500

    fix deploy paths

--- terra_ai/deploy/deploy_scripts/text_classification.py
+++ terra_ai/deploy/deploy_scripts/text_classification.py
@@ -6,9 +6,10 @@ from .cascades.create import json2cascade
 
 def predict(input_path):
     model = ""
-    for path in os.listdir(os.path.dirname(os.path.abspath(__file__))):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    for path in os.listdir(current_dir):
         if path.endswith("cascade"):
-            model = path
+            model = os.path.join(current_dir, path)
     main_block = json2cascade(model)
     main_block(input_path)
     return print(main_block[0].out[0])
