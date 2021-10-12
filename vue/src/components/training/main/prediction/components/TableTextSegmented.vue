20a32bfe3a8e80f99ae32751e174da06eda1b3a7
bondrogeen <bondrogeen@gmail.com>
2021-10-13 02:21:15 +0300

    trainings: rechange segmentation text

--- vue/src/components/training/main/prediction/components/TableTextSegmented.vue
+++ vue/src/components/training/main/prediction/components/TableTextSegmented.vue
@@ -1,7 +1,21 @@
 <template>
-  <div class="t-text-segmented" title="">
-    <div class="t-text-segmented__content" v-html="text">
-      <!-- <p class="t-text-segmented__text" v-html="text">{{ }}</p> -->
+  <div class="t-text-segmented">
+    <div class="t-text-segmented__content">
+      <div v-for="({ tags, word }, index) of arrText" :key="'word_' + index" class="t-text-segmented__word">
+        <at-tooltip v-if="!tags.includes('p1')">
+          <div class="t-text-segmented__text">{{ word }}</div>
+          <template slot="content">
+            <div class="t-text-segmented__colors" v-for="color of tags" :key="`colors_${color}`" :style="`background-color: rgb(${rgb(color)});`">{{ color }}</div>
+          </template>
+        </at-tooltip>
+        <div v-else class="t-text-segmented__text">{{ word }}</div>
+        <div
+          v-for="(tag, i) of tags"
+          :key="`tags_${index}_${i}`"
+          class="t-text-segmented__line"
+          :style="`background-color: rgb(${rgb(tag)});`"
+        ></div>
+      </div>
     </div>
   </div>
 </template>
@@ -27,36 +41,29 @@ export default {
       default: '',
     },
   },
-
   computed: {
     tags() {
       return this.tags_color?.[this.layer] || {};
     },
-    text() {
-      let text = this.value.replaceAll(/></g, '+');
-      text = text.replace(/<\/[^>]+>/g, '<>');
-      text = text.replace(/<[^>]+>/g, this.convert);
-      text = text.replaceAll('<>', '</div></div>');
-      return text;
+    arrText() {
+      let text = this.value.replaceAll(' ', '');
+      const arr = text.match(/(<[sp][0-9]>)+([^<\/>]+)(<\/[sp][0-9]>)+/g); // eslint-disable-line
+      return arr.map(item => {
+        return this.convert(item);
+      });
     },
   },
   methods: {
     rgb(tag) {
-      console.log(tag);
       const arr = this.tags[tag] || [];
       return arr.join(' ');
     },
     convert(str) {
-      str = str.replace(/[<>]/g, '');
-      str = str.split('+');
-      const tags = str
-      str = str
-        .map(item => {
-          return `<div class="t-text-segmented__line ${item}" style="background-color: rgb(${this.rgb(item)});"></div>`;
-        })
-        .join('');
-
-      return `<div class="t-text-segmented__word" title="${tags.join(' ')}">${str}<div class="t-text-segmented__text">&nbsp;`;
+      str = str.replace(/(<\/[^>]+>)+/g, '');
+      const word = str.replace(/(<[^>]+>)+/g, '');
+      str = str.replace(/></g, ',');
+      const tags = str.match(/<(.+)>/)[1].split(',');
+      return { tags, word };
     },
   },
 };
@@ -72,23 +79,38 @@ export default {
     width: 100%;
     display: flex;
     flex-wrap: wrap;
-    // justify-content: flex-start;
+    justify-content: flex-start;
     position: relative;
     align-items: flex-start;
-    text-align: start;
+    text-align: left;
+    user-select: none;
+    // gap: 5px;
   }
   &__word {
-    margin: 0 5px 0 0;
+    // margin: 0 5px 0 0;
     display: flex;
-    flex-direction: column-reverse;
+    flex: 1 1;
+    flex-direction: column;
   }
   &__text {
+    padding: 0 5px 0 0;
+  }
+  &__colors {
+    height: 20px;
+    width: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 4px;
+    margin: 3px 0;
+    font-size: 1.2em;
+    font-weight: 700;
   }
   &__line {
     display: block;
     height: 2px;
-    margin: 0 0 1px 0;
-    width: calc(100% + 5px);
+    // margin: 0 0 1px 0;
+    // width: calc(100% + 10px);
   }
 }
 </style>
