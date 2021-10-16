749b157b630e7f2bcc0c2a77c8ea89159b9a5e5e
Alexey-Krapivnitsky <cronos1009@yandex.ru>
2021-10-17 01:13:36 +0500

    reformat presets Dataframe Classification to dict

--- terra_ai/training/guinn.py
+++ terra_ai/training/guinn.py
@@ -783,11 +783,10 @@ class FitCallback(keras.callbacks.Callback):
                     columns.append(column_name[len(str(input)) + 1:])
                     if input_columns[column_name].__class__ == DatasetOutputsData:
                         predict_column = column_name[len(str(input)) + 1:]
-            if list(self.dataset.data.outputs.values())[0].task == LayerOutputTypeChoice.Regression:
-                deploy_presets_data["columns"] = columns
-                deploy_presets_data["predict_column"] = predict_column
-            else:
-                deploy_presets_data.append({"columns": columns, "predict_column": predict_column})
+            if list(self.dataset.data.outputs.values())[0].task == LayerOutputTypeChoice.Classification:
+                deploy_presets_data = {"presets": deploy_presets_data}
+            deploy_presets_data["columns"] = columns
+            deploy_presets_data["predict_column"] = predict_column if predict_column else "Предсказанные значения"
             interactive.deploy_presets_data = deploy_presets_data
         else:
             interactive.deploy_presets_data = deploy_presets_data
