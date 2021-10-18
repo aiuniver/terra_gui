95f44fcaa258b0778f6043af7cbbb14525efde66
bondrogeen <bondrogeen@gmail.com>
2021-10-18 18:11:56 +0300

    trainings: fix update

--- vue/src/components/training/main/prediction/components/TableVideo.vue
+++ vue/src/components/training/main/prediction/components/TableVideo.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Media :kind="'video'" :controls="true" :src="src"       :style="{width: '200px'}"></Media>
+    <Media :kind="'video'" :controls="true" :src="src" :style="{ width: '200px' }" :key="src"></Media>
   </div>
 </template>
 
@@ -13,10 +13,14 @@ export default {
   },
   props: {
     value: { type: String, default: '' },
+    update: {
+      type: String,
+      default: '',
+    },
   },
   computed: {
     src() {
-      return `/_media/blank/?path=${this.value}`;
+       return `/_media/blank/?path=${this.value}&r=${this.update}`;
     },
   },
   data: () => ({
