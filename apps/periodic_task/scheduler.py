ac7961b3541ff2392511d8ca8e0a04a546bfc1c5
BL146U <bl146u@gmail.com>
2021-10-18 12:22:08 +0300

    -

--- apps/periodic_task/scheduler.py
+++ apps/periodic_task/scheduler.py
@@ -14,15 +14,17 @@ executors = {
 }
 scheduler.configure(executors=executors)
 
-@scheduler.scheduled_job("interval", minutes=1)
-def scheduler_rsync():
-    date = settings.TERRA_AI_DATE_START
-    destination = Path(
-        settings.TERRA_AI_BASE_DIR,
-        "logs",
-        "gui",
-        f"{settings.USER_PORT}-{date.year}{date.month}{date.day}{date.hour}{date.minute}{date.second}.log",
-    )
-    cmd = f'rsync -avzqP -e "ssh -i {Path(settings.BASE_DIR, "rsa.key")} -o StrictHostKeyChecking=no" {Path(settings.BASE_DIR, "logs.txt")} terra_log@81.90.181.251:{destination}'
-    subprocess.call(cmd, shell=True)
-    
+
+if settings.TERRA_AI_SYNC_LOGS:
+
+    @scheduler.scheduled_job("interval", minutes=0.1)
+    def scheduler_rsync():
+        date = settings.TERRA_AI_DATE_START
+        destination = Path(
+            settings.TERRA_AI_BASE_DIR,
+            "logs",
+            "gui",
+            f"{settings.USER_PORT}-{date.year}{date.month}{date.day}{date.hour}{date.minute}{date.second}.log",
+        )
+        cmd = f'rsync -avzqP -e "ssh -i {Path(settings.BASE_DIR, "rsa.key")} -o StrictHostKeyChecking=no" {Path(settings.BASE_DIR, "logs.txt")} terra_log@81.90.181.251:{destination}'
+        subprocess.call(cmd, shell=True)
