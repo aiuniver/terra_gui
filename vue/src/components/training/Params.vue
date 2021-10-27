4ec0f955a60ab120922976a8f861f91776089880
bondrogeen <bondrogeen@gmail.com>
2021-10-27 15:20:46 +0300

    trainings: new logic fields

--- vue/src/components/training/Params.vue
+++ vue/src/components/training/Params.vue
@@ -7,93 +7,14 @@
       <div class="params__body">
         <div class="params__items">
           <at-collapse :value="collapse">
-            <at-collapse-item class="mt-3" :title="''">
-              <template v-for="(data, i) of main.fields">
-                <t-auto-field-trainings
-                  v-bind="data"
-                  :key="'main_' + i"
-                  :state="state"
-                  :inline="false"
-                  :disabled="disabledAny"
-                  @parse="parse"
-                />
-              </template>
-            </at-collapse-item>
-            <at-collapse-item class="mt-3" :title="''">
-              <div class="fit">
-                <template v-for="(data, i) of fit.fields">
+            <at-collapse-item v-for="({ name, fields }, key) of params" :key="key" class="mt-3" :title="name || ''">
+              <div class="params__fields">
+                <template v-for="(data, i) of fields">
                   <t-auto-field-trainings
                     v-bind="data"
-                    :key="'fit_' + i"
-                    class="fit__item"
-                    :state="state"
-                    :inline="true"
-                    :disabled="disabledAny"
-                    @parse="parse"
-                  />
-                </template>
-              </div>
-            </at-collapse-item>
-            <at-collapse-item class="mt-3" :title="optimizer.name">
-              <div class="optimizer">
-                <template v-for="(data, i) of optimizerFields">
-                  <t-auto-field-trainings
-                    v-bind="data"
-                    :key="'optimizer_' + i + data.parse"
-                    class="optimizer__item"
-                    :state="state"
-                    inline
-                    :disabled="disabledAny"
-                    @parse="parse"
-                  />
-                </template>
-              </div>
-            </at-collapse-item>
-            <at-collapse-item class="mt-3" :title="outputs.name">
-              <div class="blocks-layers">
-                <template v-for="(field, i) of outputs.fields">
-                  <div class="block-layers" :key="'block_layers_' + i">
-                    <div class="block-layers__header">
-                      {{ field.name }}
-                    </div>
-                    <div class="block-layers__body">
-                      <template v-for="(data, i) of field.fields">
-                        <t-auto-field-trainings
-                          v-bind="data"
-                          :key="'checkpoint_' + i + data.parse"
-                          :state="state"
-                          :inline="true"
-                          :disabled="data.disabled || disabled"
-                          @parse="parse"
-                        />
-                      </template>
-                    </div>
-                  </div>
-                </template>
-              </div>
-            </at-collapse-item>
-            <at-collapse-item class="mt-3" :title="checkpoint.name">
-              <div class="checkpoint">
-                <t-field class="checkpoint__item" inline label="Функция">
-                  <t-select-new
-                    :list="func"
-                    small
-                    update
-                    name="metric_name"
-                    :parse="'architecture[parameters][checkpoint][metric_name]'"
-                    :value="getValue"
-                    :disabled="disabled"
-                    @parse="parse"
-                  />
-                </t-field>
-                <template v-for="(data, i) of checkpoint.fields">
-                  <t-auto-field-trainings
-                    v-bind="data"
-                    :key="'outputs_' + i"
-                    class="checkpoint__item"
-                    :state="state"
-                    :inline="true"
-                    :disabled="disabled"
+                    :class="`params__fields--${key}`"
+                    :key="key + i"
+                    :inline="false"
                     @parse="parse"
                   />
                 </template>
@@ -124,7 +45,7 @@
 
 <script>
 import { debounce } from '@/utils/core/utils';
-import ser from '../../assets/js/myserialize';
+// import ser from '../../assets/js/myserialize';
 import { mapGetters } from 'vuex';
 import LoadSpiner from '@/components/forms/LoadSpiner';
 export default {
@@ -148,77 +69,11 @@ export default {
     isLearning() {
       return ['addtrain', 'training'].includes(this.status);
     },
-    disabled() {
-      return this.status !== 'no_train';
-    },
-    disabledAny() {
-      const status = this.status;
-      if (this.isLearning) {
-        return true;
-      } else {
-        if (status === 'stopped') {
-          return ['epochs'];
-        }
-      }
-      return false;
-    },
-    getValue() {
-      let data = this.trainSettings?.architecture?.parameters?.outputs || [];
-      data = data?.[this.metricData]?.metrics || [];
-      this.saveValue(data);
-      return this.state?.['architecture[parameters][checkpoint][metric_name]'] || data[0] || '';
-    },
-    state: {
-      set(value) {
-        this.$store.dispatch('trainings/setStateParams', value);
-      },
-      get() {
-        return this.$store.getters['trainings/getStateParams'];
-      },
-    },
-    trainSettings: {
-      set(value) {
-        this.$store.dispatch('trainings/setTrainSettings', value);
-      },
-      get() {
-        return this.$store.getters['trainings/getTrainSettings'];
-      },
-    },
-    main() {
-      return this.params?.main || {};
-    },
-    fit() {
-      return this.params?.fit || {};
-    },
-    outputs() {
-      return this.params?.outputs || {};
-    },
-    optimizerFields() {
-      return this.params?.optimizer?.fields?.[this.optimizerValue] || [];
-    },
-    optimizer() {
-      return this.params?.optimizer || {};
-    },
-    checkpoint() {
-      return this.params?.checkpoint || {};
-    },
     statusTrain() {
       return this.$store.getters['trainings/getStatusTrain'];
     },
-    func() {
-      let data = this.trainSettings?.architecture?.parameters?.outputs || [];
-      data = data?.[this.metricData]?.metrics || [];
-      data = data.map(item => {
-        return { label: item, value: item };
-      });
-      return data;
-    },
   },
   methods: {
-    saveValue([value]) {
-      ser(this.trainSettings, 'architecture[parameters][checkpoint][metric_name]', value);
-      this.trainSettings = { ...this.trainSettings };
-    },
     btnEvent(key) {
       if (key === 'train') {
         this.start();
@@ -233,11 +88,7 @@ export default {
         this.save();
       }
     },
-    click(e) {
-      console.log(e);
-    },
     async start() {
-      // console.log(JSON.stringify(this.trainSettings, null, 2));
       const res = await this.$store.dispatch('trainings/start', this.trainSettings);
       if (res) {
         const { data } = res;
@@ -251,8 +102,7 @@ export default {
       // console.log(res);
     },
     async stop() {
-      // this.debounce(false);
-      this.stopLearning = true
+      this.stopLearning = true;
       await this.$store.dispatch('trainings/stop', {});
     },
     async clear() {
@@ -277,40 +127,37 @@ export default {
       }
     },
     parse({ parse, value, name }) {
-      // console.log({ parse, value, name });
-      ser(this.trainSettings, parse, value);
-      this.trainSettings = { ...this.trainSettings };
-      if (name === 'architecture_parameters_checkpoint_layer') {
-        this.metricData = value;
-        if (value) {
-          this.state = { [`${parse}`]: value };
-        }
-      } else {
-        this.state = { [`${parse}`]: value };
-      }
-      if (name === 'optimizer') {
-        this.optimizerValue = value;
-      }
+      console.log({ parse, value, name });
+      // ser(this.trainSettings, parse, value);
+      // this.trainSettings = { ...this.trainSettings };
+      // if (name === 'architecture_parameters_checkpoint_layer') {
+      //   this.metricData = value;
+      //   if (value) {
+      //     this.state = { [`${parse}`]: value };
+      //   }
+      // } else {
+      //   this.state = { [`${parse}`]: value };
+      // }
+      // if (name === 'optimizer') {
+      //   this.optimizerValue = value;
+      // }
     },
   },
   created() {
     this.debounce = debounce(status => {
-      // console.log(status);
       if (status) {
         this.progress();
       }
     }, 1000);
-
-    // console.log(this.isLearning);
     this.debounce(this.isLearning);
-    const settings = localStorage.getItem('settingsTrainings');
-    if (settings) {
-      try {
-        this.state = JSON.parse(settings);
-      } catch (error) {
-        console.warn(error);
-      }
-    }
+    // const settings = localStorage.getItem('settingsTrainings');
+    // if (settings) {
+    //   try {
+    //     this.state = JSON.parse(settings);
+    //   } catch (error) {
+    //     console.warn(error);
+    //   }
+    // }
   },
   beforeDestroy() {
     this.debounce(false);
@@ -377,23 +224,20 @@ export default {
   }
   &__items {
     padding-bottom: 20px;
-    &--item {
-      padding: 20px;
+  }
+  &__fields {
+    display: flex;
+    flex-wrap: wrap;
+    div {
+      width: 50%;
+    }
+    &--main {
+      width: 100% !important;
     }
   }
 }
 
 .btn-spiner {
   margin-top: 10px;
-}
-
-.fit,
-.optimizer,
-.checkpoint {
-  display: flex;
-  flex-wrap: wrap;
-  &__item {
-    width: 50%;
-  }
 }
 </style>
