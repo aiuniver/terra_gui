a39e2c0d6119561a29facacfde7d278800dc456a
bondrogeen <bondrogeen@gmail.com>
2021-11-01 18:49:36 +0300

    trainings: add button save

--- vue/src/components/training/Params.vue
+++ vue/src/components/training/Params.vue
@@ -60,11 +60,14 @@
         v-for="({ title, visible }, key) of button"
         :key="key"
         class="params__btn"
-        :class="{ params__save: key === 'clear' }"
       >
-        <t-button v-if="key !== 'save'" :disabled="!visible" @click="btnEvent(key)">{{ title }}</t-button>
+        <t-button :disabled="!visible" @click="btnEvent(key)">{{ title }}</t-button>
       </div>
+      <!-- <div class="params__btn">
+        <t-button :disabled="false" @click="btnEvent('save')">{{ 'Сохранить' }}</t-button>
+      </div> -->
     </div>
+    <SaveTrainings v-model="dialogSave" />
   </div>
 </template>
 
@@ -73,10 +76,12 @@ import { debounce } from '@/utils/core/utils';
 import ser from '../../assets/js/myserialize';
 import { mapGetters } from 'vuex';
 import LoadSpiner from '@/components/forms/LoadSpiner';
+import SaveTrainings from '@/components/app/modal/SaveTrainings';
 export default {
   name: 'params-traning',
   components: {
     LoadSpiner,
+    SaveTrainings,
   },
   data: () => ({
     collapse: ['main', 'fit', 'outputs', 'checkpoint', 'yolo'],
@@ -84,11 +89,10 @@ export default {
     metricData: '',
     debounce: null,
     stopLearning: false,
+    dialogSave: false,
     trainSettings: {},
     key: '1212',
-    doNotSave: [
-      'architecture[parameters][checkpoint][metric_name]'
-    ]
+    doNotSave: ['architecture[parameters][checkpoint][metric_name]'],
   }),
   computed: {
     ...mapGetters({
@@ -148,8 +152,9 @@ export default {
     async clear() {
       await this.$store.dispatch('trainings/clear', {});
     },
-    async save() {
-      await this.$store.dispatch('trainings/save', {});
+    save() {
+      this.dialogSave = true;
+      // await this.$store.dispatch('trainings/save', {});
     },
     async progress() {
       const res = await this.$store.dispatch('trainings/progress', {});
@@ -167,7 +172,7 @@ export default {
       }
     },
     parse({ parse, value, changeable, mounted }) {
-    // parse({ parse, value, name, changeable, mounted }) {
+      // parse({ parse, value, name, changeable, mounted }) {
       // console.log({ parse, value, name, changeable, mounted });
       ser(this.trainSettings, parse, value);
       this.trainSettings = { ...this.trainSettings };
