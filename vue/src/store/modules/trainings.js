30b1a099de85f8478083b90130fbbddb78986624
bondrogeen <bondrogeen@gmail.com>
2021-11-08 19:53:15 +0300

    trainings: change struct

--- vue/src/store/modules/trainings.js
+++ vue/src/store/modules/trainings.js
@@ -10,41 +10,71 @@ export default {
   namespaced: true,
   state: () => ({
     collapse: ['0', '1'],
-    params: {},
+    // params: {},
     toolbar,
     stateParams: {},
     predict: {},
     info: '',
     states: {},
     trainSettings: {},
-    trainData: {},
+    // trainData: {},
     // trainData: process.env.NODE_ENV === 'development' ? data : {},
-    trainUsage: {},
+    // trainUsage: {},
     statusTrain: 'no_train',
-    training: {
-      base: {},
-      interactive: {},
-      state: {}
-    },
+    // training: {
+    //   base: {},
+    //   interactive: {},
+    //   state: {}
+    // },
+
+    form: {},
+    state: {},
+    result: {},
+    progress: {},
     interactive: {},
-    architecture: '',
+
+    // architecture: '',
     largeImgSrc: null
   }),
   mutations: {
-    SET_ARCHITECTURE (state, value) {
-      state.architecture = value;
+    SET_FORM (state, value) {
+      state.form = value;
+    },
+    SET_STATE (state, value) {
+      state.state = value;
     },
     SET_INTERACTIV (state, value) {
-      state.interactive = { ...value };
+      state.interactive = value;
     },
+    SET_PROGRESS (state, value) {
+      state.progress = value;
+    },
+    SET_RESULT (state, value) {
+      state.result = value;
+    },
+
+
+
+
+
+
+
+
+
+
+
+    // SET_ARCHITECTURE (state, value) {
+    //   state.architecture = value;
+    // },
+
     SET_TRAIN_SETTINGS (state, value) {
       state.trainSettings = { ...value };
     },
-    SET_PARAMS (state, value) {
-      state.params = { ...value };
-    },
+    // SET_PARAMS (state, value) {
+    //   state.params = { ...value };
+    // },
     SET_CONFIG (state, value) {
-      state.training = { ...value };
+      // state.training = { ...value };
       state.interactive = JSON.parse(JSON.stringify(value.interactive))
     },
     SET_STATE_PARAMS (state, value) {
@@ -53,19 +83,16 @@ export default {
     SET_INFO (state, value) {
       state.info = value;
     },
-    SET_STATE (state, value) {
-      state.training.state = value;
-      state.training = { ...state.training }
-    },
+
     SET_PREDICT (state, value) {
       state.predict = { ...value };
     },
-    SET_TRAIN (state, value) {
-      state.trainData = { ...value };
-    },
-    SET_TRAIN_USAGE (state, value) {
-      state.trainUsage = { ...value };
-    },
+    // SET_TRAIN (state, value) {
+    //   state.trainData = { ...value };
+    // },
+    // SET_TRAIN_USAGE (state, value) {
+    //   state.trainUsage = { ...value };
+    // },
     SET_COLLAPSE (state, value) {
       state.collapse = [...value];
     },
@@ -77,32 +104,47 @@ export default {
     },
   },
   actions: {
-    setState ({ dispatch, commit }, res) {
-      // console.log(res)
-      if (res && res?.data) {
-        const state = res?.data?.data?.state || res?.data.state
-        const base = res?.data?.form?.base
-        if (state) {
-          commit("SET_STATE", state);
-          if (base) {
-            console.log(base)
-            commit("SET_PARAMS", base);
-          }
-          dispatch('setStatusTrain', state.status);
-        }
+    parseStruct ({ commit }, { form, interactive, progress, state, result, test }) {
+      console.log(form, interactive, progress, state)
+      if (form) commit("SET_FORM", form)
+      if (state) commit("SET_STATE", state)
+      if (state?.status) commit("SET_STATUS_TRAIN", state.status)
+      if (interactive) commit("SET_INTERACTIV", interactive)
+      if (progress) commit("SET_PROGRESS", state)
+      if (result) commit("SET_RESULT", result)
+      if (test) {
+        commit('sdsdd', 1)
       }
     },
-    async start ({ state: { training: { state: { status } } }, dispatch }, parse) {
+    // setState ({ dispatch, commit }, res) {
+    //   // console.log(res)
+    //   if (res && res?.data) {
+    //     const state = res?.data?.data?.state || res?.data.state
+    //     const base = res?.data?.form?.base
+    //     if (state) {
+    //       commit("SET_STATE", state);
+    //       if (base) {
+    //         console.log(base)
+    //         commit("SET_PARAMS", base);
+    //       }
+    //       dispatch('setStatusTrain', state.status);
+    //     }
+    //   }
+    // },
+    async start ({ state: { state: { status } }, dispatch }, parse) {
+      console.log('1')
       let isValid = true
       if (status === 'no_train') {
+        console.log('2')
         const valid = await dispatch('modeling/validateModel', {}, { root: true })
         isValid = !Object.values(valid || {}).filter(item => item).length
         dispatch('setTrainData', {});
       }
+      console.log('3')
       if (isValid) {
         dispatch('setStatusTrain', 'start');
         let data = JSON.parse(JSON.stringify(parse))
-        // console.log(data)
+        console.log(data)
         const arht = data.architecture.parameters.outputs || []
         data.architecture.parameters.outputs = arht.map((item, index) => {
           return item ? { id: index, ...item } : null
@@ -111,21 +153,24 @@ export default {
         const res = await dispatch('axios', { url: '/training/start/', data }, { root: true });
         if (res && res?.data) {
           await dispatch('projects/get', {}, { root: true })
-          dispatch('setState', res);
+          // dispatch('setState', res);
         } else {
           dispatch('setStatusTrain', 'no_train');
         }
+        dispatch('parseStruct', res?.data || {});
         return res
       }
       return null
     },
     async stop ({ dispatch }, data) {
       const res = await dispatch('axios', { url: '/training/stop/', data }, { root: true });
-      dispatch('setState', res);
+      // dispatch('setState', res);
+      dispatch('parseStruct', res?.data || {});
       return res
     },
     async save ({ dispatch }, data) {
       const res = await dispatch('axios', { url: '/training/save/', data }, { root: true });
+      // dispatch('parseStruct', res?.data || {});
       return res
     },
     async update ({ commit, dispatch }, parse) {
@@ -140,16 +185,18 @@ export default {
       if (res) {
         const { data, error } = res
         if (data && !error) {
-          commit("SET_PARAMS", data.form.base);
+          // commit("SET_PARAMS", data.form.base);
           commit("SET_CONFIG", data.data);
         }
       }
+      dispatch('parseStruct', res?.data || {});
       return res
     },
     async clear ({ dispatch }, data) {
       const res = await dispatch('axios', { url: '/training/clear/', data }, { root: true });
-      dispatch('setState', res);
+      // dispatch('setState', res);
       dispatch('resetTraining', {});
+      dispatch('parseStruct', res?.data || {});
       return res
     },
     async interactive ({ state: { interactive }, dispatch }, part) {
@@ -166,14 +213,17 @@ export default {
     },
     async progress ({ dispatch }, data) {
       const res = await dispatch('axios', { url: '/training/progress/', data }, { root: true });
+      console.log(res)
       if (res) {
-        const { data, error } = res.data;
+        const { data, error } = res;
         if (data) {
-          const { info, train_data, train_usage } = data;
-          if (info) dispatch('setInfo', info);
-          dispatch('setState', res);
-          if (train_data) dispatch('setTrainData', train_data);
-          if (train_usage) dispatch('setTrainUsage', train_usage);
+          console.log(data)
+          dispatch('parseStruct', data || {});
+          // const { info, train_data, train_usage } = data;
+          // if (info) dispatch('setInfo', info);
+          // dispatch('setState', res);
+          // if (train_data) dispatch('setTrainData', train_data);
+          // if (train_usage) dispatch('setTrainUsage', train_usage);
         }
         if (error) {
           dispatch('messages/setMessage', { error: error }, { root: true });
@@ -185,14 +235,14 @@ export default {
     async resetTraining ({ dispatch }) {
       localStorage.removeItem('settingsTrainings');
       dispatch('messages/resetProgress', {}, { root: true });
-      dispatch('setTrainData', {});
-      dispatch('setTrainUsage', {});
+      // dispatch('setTrainData', {});
+      // dispatch('setTrainUsage', {});
       await dispatch('projects/get', {}, { root: true })
     },
     async resetAllTraining ({ commit, dispatch }) {
       dispatch('resetTraining', {});
-      dispatch('setTrainSettings', {});
-      dispatch('setTrainSettings', {});
+      // dispatch('setTrainSettings', {});
+      // dispatch('setTrainSettings', {});
       commit("SET_STATE_PARAMS", {});
     },
     setDrawer ({ commit }, data) {
@@ -207,15 +257,15 @@ export default {
     // setStates({ commit }, data) {
     //   commit("SET_STATES", data);
     // },
-    setTrainData ({ commit }, data) {
-      commit("SET_TRAIN", data);
-    },
+    // setTrainData ({ commit }, data) {
+    //   commit("SET_TRAIN", data);
+    // },
     setPredict ({ commit }, data) {
       commit("SET_PREDICT", data);
     },
-    setTrainUsage ({ commit }, data) {
-      commit("SET_TRAIN_USAGE", data);
-    },
+    // setTrainUsage ({ commit }, data) {
+    //   commit("SET_TRAIN_USAGE", data);
+    // },
     setTrainDisplay ({ commit }, data) {
       commit("SET_TRAIN_DISPLAY", data);
     },
@@ -240,7 +290,7 @@ export default {
     getTrainSettings: ({ trainSettings }) => {
       return trainSettings
     },
-    getArchitecture: ({ architecture }) => {
+    getArchitecture: ({ form: { architecture } }) => {
       return architecture
     },
     getStatusTrain: ({ statusTrain }) => {
@@ -258,29 +308,29 @@ export default {
     getСollapse ({ collapse }) {
       return collapse || []
     },
-    getInteractive ({ training: { interactive } }) {
+    getInteractive ({ interactive }) {
       return interactive || {}
     },
-    getStatus ({ training: { state: { status } } }) {
+    getStatus ({ state: { status } }) {
       return status || ''
     },
-    getButtons ({ training: { state: { buttons } } }) {
+    getButtons ({ state: { buttons } }) {
       return buttons
     },
-    getOutputs ({ training: { base } }) {
+    getOutputs ({ form: { base } }) {
       return base?.architecture?.parameters?.outputs || []
     },
-    getParams ({ params }) {
-      return params || []
+    getParams ({ form }) {
+      return form?.base || []
     },
     getToolbar ({ toolbar }) {
       return toolbar;
     },
-    getTrainUsage: ({ trainUsage }) => {
-      return trainUsage || {};
+    getTrainUsage: ({ result: { train_usage } }) => {
+      return train_usage || {};
     },
-    getTrainData: ({ trainData }) => (key) => {
-      return trainData?.[key];
+    getTrainData: ({ result: { train_data } }) => (key) => {
+      return train_data?.[key];
     },
     getPredict ({ predict }) {
       return predict || {}
