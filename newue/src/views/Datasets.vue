89d0a36861819a1a24896a0e20db5e35e221d018
Ivan <45362427+Efcolipt@users.noreply.github.com>
2021-11-10 05:16:09 +0300

    add datasets

---
+++ newue/src/views/Datasets.vue
@@ -0,0 +1,29 @@
+<template>
+  <main class="page-datasets">
+      <SvgContainer name="arrow-right-active-fill" />
+      <SvgContainer name="arrow-right-outline" />
+      <SvgContainer name="arrow-right-fill" />
+      <SvgContainer name="notification-outline" />
+      <SvgContainer name="help-circle" />
+      <SvgContainer name="google-drive" />
+      <SvgContainer name="file-outline" />
+      <SvgContainer name="folder" />
+      <SvgContainer name="folder-add" />
+      <SvgContainer name="folder-save" />
+      <SvgContainer name="folder-validate" />
+      <SvgContainer name="folder-remove" />
+      <SvgContainer name="sloy-start-add" />
+      <SvgContainer name="sloy-middle-add" />
+      <SvgContainer name="sloy-end-add" />
+  </main>
+</template>
+
+<script>
+import SvgContainer from "@/components/app/SvgContainer"
+export default {
+  name: 'Datasets',
+  components: {
+      SvgContainer
+  }
+};
+</script>
