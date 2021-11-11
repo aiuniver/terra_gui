a6209240db9351b0e780e4719144b062d74031b6
Ivan <45362427+Efcolipt@users.noreply.github.com>
2021-11-11 04:57:01 +0300

    add panel to page

--- newue/src/views/Datasets.vue
+++ newue/src/views/Datasets.vue
@@ -1,29 +1,128 @@
 <template>
   <main class="page-datasets">
-      <SvgContainer name="arrow-right-active-fill" />
-      <SvgContainer name="arrow-right-outline" />
-      <SvgContainer name="arrow-right-fill" />
-      <SvgContainer name="notification-outline" />
-      <SvgContainer name="help-circle" />
-      <SvgContainer name="google-drive" />
-      <SvgContainer name="file-outline" />
-      <SvgContainer name="folder" />
-      <SvgContainer name="folder-add" />
-      <SvgContainer name="folder-save" />
-      <SvgContainer name="folder-validate" />
-      <SvgContainer name="folder-remove" />
-      <SvgContainer name="sloy-start-add" />
-      <SvgContainer name="sloy-middle-add" />
-      <SvgContainer name="sloy-end-add" />
+    <div class="page-datasets__inner">
+      <div>asd</div>
+      <BasePanel>
+        <BasePanelContent>
+          <template #header>Данные</template>
+          <template #content>
+            <div class="mb-4">Выберите папку/файл</div>
+            <FileManager @chooseFile="chooseFile" :list="list" />
+          </template>
+        </BasePanelContent>
+        <BasePanelContent>
+          <template #header>Предпросмотр</template>
+          <template #content>
+            <Preview :list="preview" />
+          </template>
+        </BasePanelContent>
+        <BasePanelContent>
+          <template #header>Настройки</template>
+          <template #content>
+            
+          </template>
+        </BasePanelContent>
+      </BasePanel>
+    </div>
   </main>
 </template>
 
 <script>
-import SvgContainer from "@/components/app/SvgContainer"
+
+import BasePanel from '@/components/datasets/components/panel/BasePanel';
+import BasePanelContent from '@/components/datasets/components/panel/BasePanelContent';
+
+import FileManager from '@/components/datasets/components/FileManager';
+import Preview from '@/components/datasets/components/Preview';
+
 export default {
-  name: 'Datasets',
   components: {
-      SvgContainer
-  }
+    BasePanelContent,
+    BasePanel,
+    Preview,
+    FileManager,
+  },
+
+  name: 'Datasets',
+  methods: {
+    chooseFile(item) {
+      console.log(item);
+    },
+  },
+  data: () => ({
+    preview: [
+      {
+        path: 'http://u01.appmifile.com/images/2017/04/15/c9b37bf4-2bed-466f-b2a1-e3bee2e54d7c.jpg',
+        label: 'Мерседес 1',
+      },
+      {
+        path: 'https://i.pinimg.com/originals/b3/cf/82/b3cf8221bf35baf3d4faa68473811fc9.jpg',
+        label: 'Мерседес 2',
+      },
+      {
+        path: 'https://storge.pic2.me/c/1360x800/510/029.jpg',
+        label: 'Мерседес 3',
+      },
+    ],
+    list: [
+      {
+        id: 1,
+        label: 'Мерседес',
+        list: [
+          {
+            label: 'model_1_10_06_mnistasd',
+          },
+          {
+            label: 'model_asda_06_mnisasdt',
+          },
+          {
+            label: 'moasdl_asdasdasda_06_mnisasdtasd',
+          },
+        ],
+      },
+      {
+        id: 2,
+        label: 'Рено',
+        list: [
+          {
+            label: 'model_1_10_06_mnist23d23',
+          },
+        ],
+      },
+      {
+        id: 3,
+        label: 'Рено',
+        list: [
+          {
+            label: 'model_1_10_06_mnistd23dsae',
+          },
+          {
+            label: 'model_1_10_06_mnistfe24c',
+          },
+          {
+            label: 'model_1_10_06_mnistbg5rgvt',
+          },
+          {
+            label: 'model_1_10_06_mnisttbyg3v5',
+          },
+          {
+            label: 'model_1_10_06_mnistn5h4e',
+          },
+        ],
+      },
+    ],
+  }),
 };
 </script>
+
+<style lang="scss" scoped>
+.page {
+  &-datasets {
+    &__inner::v-deep {
+      display: flex;
+      height: 100%;
+      justify-content: space-between;
+    }
+  }
+}
+</style>
