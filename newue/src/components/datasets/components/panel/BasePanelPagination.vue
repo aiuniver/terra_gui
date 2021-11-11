49ca2ac1e7b9930ae2aefe947a6081a2b21a0239
Ivan <45362427+Efcolipt@users.noreply.github.com>
2021-11-11 04:56:55 +0300

    add panel

---
+++ newue/src/components/datasets/components/panel/BasePanelPagination.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="panel-pagination flex align-center">
+    <button class="btn panel-pagination-btn">
+      <SvgContainer name="arrow-left-longer-big" />
+    </button>
+    <div class="panel-pagination-container">
+      <div class="panel-pagination-container__list flex align-center">
+        <div class="panel-pagination-container__list-item"></div>
+        <div class="panel-pagination-container__list-item panel-pagination-container__list-item--active"></div>
+        <div class="panel-pagination-container__list-item"></div>
+        <div class="panel-pagination-container__list-item"></div>
+      </div>
+      <div class="panel-pagination-container__header">
+        <span>Входные данные</span>
+      </div>
+    </div>
+    <DButton style="width: 40%" color="secondary" direction="left" text="Далее" />
+  </div>
+</template>
+
+<script>
+import SvgContainer from '@/components/app/SvgContainer';
+import DButton from '@/components/global/forms/components/DButton';
+
+export default {
+  components: {
+    DButton,
+    SvgContainer,
+  },
+  name: 'BasePanelPagination',
+};
+</script>
+
+<style lang="scss" scoped>
+.panel-pagination {
+  justify-content: space-between;
+  &-container {
+    margin: 0 10px;
+    text-align: center;
+    width: 200px;
+    &__header {
+      margin-top: 15px;
+    }
+    &__list {
+      justify-content: center;
+      &-item {
+        background: $color-gray-blue;
+        border-radius: 50%;
+        display: block;
+        margin: 0 auto;
+        margin-right: 25px;
+        width: 5px;
+        height: 5px;
+        &--active {
+          width: 10px;
+          height: 10px;
+          background: $color-light-blue;
+        }
+      }
+    }
+  }
+}
+</style>
