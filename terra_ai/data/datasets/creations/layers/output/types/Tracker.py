e6103e2684399966fba77d9b3e4f2cda0bd032f6
theones777 <kononov52rus@gmail.com>
2021-11-12 13:53:27 +0300

    tracker

--- terra_ai/data/datasets/creations/layers/output/types/Tracker.py
+++ terra_ai/data/datasets/creations/layers/output/types/Tracker.py
@@ -1,5 +1,7 @@
-from terra_ai.data.datasets.creations.layers.extra import SourcesPathsData
+from typing import Optional
+from pydantic.types import PositiveInt
+from terra_ai.data.datasets.creations.layers.extra import SourcesPathsData, ColumnProcessingData
 
 
-class ParametersData(SourcesPathsData):
-    pass
+class ParametersData(SourcesPathsData, ColumnProcessingData):
+    put: Optional[PositiveInt]
