1451d991f1f70d8f94691a41b6c6ed0fda9b53b3
Ivan <45362427+Efcolipt@users.noreply.github.com>
2021-11-19 08:51:05 +0300

    Fix  training_available on datasets

--- vue/src/components/datasets/cards/CardDataset.vue
+++ vue/src/components/datasets/cards/CardDataset.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="['dataset-card-item', { active: dataset.active && !loaded, selected: loaded}]" :title="dataset.name">
-    <div :class="['dataset-card', {'disabled': dataset.training_available ? !dataset.training_available : false }]" @click.stop="$emit('click', dataset, cardIndex)">
+    <div :class="['dataset-card', {'disabled': !dataset.training_available}]" @click.stop="$emit('click', dataset, cardIndex)">
       <div class="card-title">{{ dataset.name }}</div>
       <div class="card-body" @click="click">
         <div v-if="dataset.tags.length <= 4">
