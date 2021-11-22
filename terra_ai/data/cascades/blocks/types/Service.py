30c054b4b295e2039f0ab89faa6b6870e1807f8b
BL146U <bl146u@gmail.com>
2021-11-22 13:29:36 +0300

    -

--- terra_ai/data/cascades/blocks/types/Service.py
+++ terra_ai/data/cascades/blocks/types/Service.py
@@ -17,12 +17,32 @@ class ParametersMainData(BaseMixinData):
     max_age: Optional[PositiveInt] = 4
     min_hits: Optional[PositiveInt] = 4
     distance_threshold: Optional[confloat(gt=0, lt=1)] = 0.4
-    metric: Optional[BlockServiceBiTBasedTrackerMetricChoice] = BlockServiceBiTBasedTrackerMetricChoice.euclidean
-    version: Optional[BlockServiceYoloV5VersionChoice] = BlockServiceYoloV5VersionChoice.Small
+    metric: Optional[
+        BlockServiceBiTBasedTrackerMetricChoice
+    ] = BlockServiceBiTBasedTrackerMetricChoice.euclidean
+    version: Optional[
+        BlockServiceYoloV5VersionChoice
+    ] = BlockServiceYoloV5VersionChoice.Small
     render_img: Optional[bool] = True
+
+    def __init__(self, **data):
+        _type = data.get("type")
+        _keys = ["group", "type"]
+        if _type == BlockServiceTypeChoice.Sort:
+            _keys += ["max_age", "min_hits"]
+        elif _type == BlockServiceTypeChoice.BiTBasedTracker:
+            _keys += ["max_age", "distance_threshold", "metric"]
+        elif _type == BlockServiceTypeChoice.YoloV5:
+            _keys += ["version", "render_img"]
+        data = dict(filter(lambda item: item[0] in _keys, data.items()))
+        super().__init__(**data)
 
     @validator("type")
     def _validate_type(cls, value: BlockServiceTypeChoice) -> BlockServiceTypeChoice:
+        for name, item in cls.__fields__.items():
+            if name in ["group", "type"]:
+                continue
+            cls.__fields__[name].required = False
         if value == BlockServiceTypeChoice.Sort:
             cls.__fields__["max_age"].required = True
             cls.__fields__["min_hits"].required = True
@@ -32,6 +52,5 @@ class ParametersMainData(BaseMixinData):
             cls.__fields__["metric"].required = True
         if value == BlockServiceTypeChoice.YoloV5:
             cls.__fields__["version"].required = True
-            cls.__fields__["render_img"].required = True
-     
+
         return value
