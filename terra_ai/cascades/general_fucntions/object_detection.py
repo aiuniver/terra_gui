126e2b01bf4003a0ebd9a67a0361b5bb2f09c222
Pushikic <55259402+Pushikic@users.noreply.github.com>
2021-11-30 02:35:48 +0300

    -

--- terra_ai/cascades/general_fucntions/object_detection.py
+++ terra_ai/cascades/general_fucntions/object_detection.py
@@ -122,7 +122,8 @@ def plot_b_boxes(classes, colors=None, line_thickness=None):
 
             # Plots one bounding box on image img
             tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
-
+            if tl < 0:
+                tl = 1
             for x in bboxes:
                 cl = int(x[-1])
                 if brawl_color:
@@ -130,19 +131,14 @@ def plot_b_boxes(classes, colors=None, line_thickness=None):
                 else:
                     color = colors[0]
                 c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
-                cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+
+                cv2.rectangle(img, c1, c2, color, tl, cv2.LINE_AA)
 
                 t_size = (114, 16)
-                c2 = c1[0] + t_size[0] + 5, c1[1] - t_size[1] - 5
-                c1 = c1[0], c1[1] - 3
-                if c2[0] < 0:
-                    c2 = 0
-                elif c1[0] < 0:
-                    c1 = 0
-                elif c2[1] < 0:
-                    c2 = 0
-                elif c1[1] < 0:
-                    c1 = 0
+                c2 = c1[0] + t_size[0] + 5 if (c1[0] + t_size[0] + 5) > 0 else 0, \
+                     c1[1] - t_size[1] - 5 if (c1[1] - t_size[1] - 5) > 0 else 0
+                c1 = c1[0] if (c1[0]) > 0 else 0, c1[1] - 3 if (c1[1] - 3) > 0 else 0
+
                 cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
 
                 pil_img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
@@ -155,6 +151,7 @@ def plot_b_boxes(classes, colors=None, line_thickness=None):
 
                 img = cv2.cvtColor(np.asarray(pil_img), cv2.COLOR_RGB2BGR)
         return img
+
     return fun
 
 
