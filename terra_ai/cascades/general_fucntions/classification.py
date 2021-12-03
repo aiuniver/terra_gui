a3d0da4c78de3e28570ab6249ccab7d1dbd44ca1
Pushikic <55259402+Pushikic@users.noreply.github.com>
2021-12-04 01:52:12 +0300

    round probability cls

--- terra_ai/cascades/general_fucntions/classification.py
+++ terra_ai/cascades/general_fucntions/classification.py
@@ -8,12 +8,11 @@ def main(**params):
     def fun(acc):
         acc *= 100
         acc = np.array(acc)
-        acc = acc.astype(np.int)
         if len(acc) == 1:
             acc = acc[0]
         else:
             acc = np.mean(acc, axis=0)
-
+        acc = acc.round().astype(np.int)
         out = list(zip(classes_names, acc))
         return sorted(out, key=lambda x: x[-1], reverse=True)
 
