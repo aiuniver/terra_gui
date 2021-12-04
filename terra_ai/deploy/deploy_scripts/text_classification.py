859255cf88d0f136566715ee2bee705749a9f5ba
Pushikic <55259402+Pushikic@users.noreply.github.com>
2021-12-04 12:24:39 +0300

    cut len out deploy classification

--- terra_ai/deploy/deploy_scripts/text_classification.py
+++ terra_ai/deploy/deploy_scripts/text_classification.py
@@ -9,7 +9,10 @@ def predict(input_path):
             model = os.path.join(current_dir, path)
     main_block = json2cascade(model)
     main_block(input_path)
-    return print(main_block[0].out[0])
+    out = main_block[0].out
+    while len(out) == 1:
+        out = out[0]
+    return print(out)
 
 
 if __name__ in ["__main__", "script"]:
