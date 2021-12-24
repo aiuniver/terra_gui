83c44d640d1d66d304cfd925a1b5408f8b690ae8
bl146u <bl146u@gmail.com>
2021-12-24 17:09:50 +0300

    -

--- terra_ai/__init__.py
+++ terra_ai/__init__.py
@@ -1,6 +1,9 @@
+import os
+
 from pathlib import Path
 from dotenv import load_dotenv
 
 
-def settings_load(path: str = ".env"):
+def settings_load(path: str = ".env", **kwargs):
     load_dotenv(str(Path(path).absolute()))
+    os.environ.update(**kwargs)
