7e86e932dc6946ec9aa33f32ddfa5e3a7d0326d4
Danil Popov <zabastx@gmail.com>
2021-12-27 18:20:58 +0300

    деплой: кроссбраузерный аудио плеер

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,7 +1,7 @@
 const proxy = {
   bondrogeen: 'http://192.168.1.47:8099/',
   MacBookPro: 'http://localhost:8001/',
-  zabastx: 'http://localhost:8000/',
+  zabastx: 'http://104.248.93.132/',
 };
 module.exports = {
   productionSourceMap: false,
