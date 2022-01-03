2aca6e3054978c4e0799449f1d33fed5d5abb178
bl146u <bl146u@gmail.com>
2022-01-03 11:42:56 +0300

    Вернул конфигурацию логирования

--- config/settings.py
+++ config/settings.py
@@ -159,70 +159,70 @@ USER_SESSION = env.str("USER_SESSION")
 
 # Logging
 
-# LOGGING = {
-#     "version": 1,
-#     "disable_existing_loggers": False,
-#     "filters": {
-#         "require_debug_false": {
-#             "()": "django.utils.log.RequireDebugFalse",
-#         },
-#         "require_debug_true": {
-#             "()": "django.utils.log.RequireDebugTrue",
-#         },
-#     },
-#     "formatters": {
-#         "django.server": {
-#             "()": "django.utils.log.ServerFormatter",
-#             "format": "[{server_time}] {message}",
-#             "style": "{",
-#         },
-#         "terra.console": {
-#             "()": "apps.api.logging.TerraConsoleFormatter",
-#             "format": "[{server_time}] \033[{levelname_color}mTerra [{levelname_short}]\033[0m: {message}",
-#             "style": "{",
-#         },
-#     },
-#     "handlers": {
-#         "console": {
-#             "level": "INFO",
-#             "filters": ["require_debug_true"],
-#             "class": "logging.StreamHandler",
-#         },
-#         "django.server": {
-#             "level": "INFO",
-#             "class": "logging.StreamHandler",
-#             "formatter": "django.server",
-#         },
-#         "terra.console": {
-#             "level": "DEBUG",
-#             "class": "apps.api.logging.TerraConsoleHandler",
-#             "formatter": "terra.console",
-#         },
-#         "terra.request_catcher": {
-#             "level": "DEBUG",
-#             "class": "apps.api.logging.TerraLogsCatcherHandler",
-#             "formatter": "terra.console",
-#         },
-#         "mail_admins": {
-#             "level": "ERROR",
-#             "filters": ["require_debug_false"],
-#             "class": "django.utils.log.AdminEmailHandler",
-#         },
-#     },
-#     "loggers": {
-#         "django": {
-#             "handlers": ["console", "mail_admins"],
-#             "level": "INFO",
-#         },
-#         "django.server": {
-#             "handlers": ["django.server"],
-#             "level": "INFO",
-#             "propagate": False,
-#         },
-#         "terra": {
-#             "handlers": ["terra.console", "terra.request_catcher"],
-#             "level": "DEBUG",
-#             "propagate": False,
-#         },
-#     },
-# }
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse",
+        },
+        "require_debug_true": {
+            "()": "django.utils.log.RequireDebugTrue",
+        },
+    },
+    "formatters": {
+        "django.server": {
+            "()": "django.utils.log.ServerFormatter",
+            "format": "[{server_time}] {message}",
+            "style": "{",
+        },
+        "terra.console": {
+            "()": "apps.api.logging.TerraConsoleFormatter",
+            "format": "[{server_time}] \033[{levelname_color}mTerra [{levelname_short}]\033[0m: {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            "filters": ["require_debug_true"],
+            "class": "logging.StreamHandler",
+        },
+        "django.server": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+            "formatter": "django.server",
+        },
+        "terra.console": {
+            "level": "DEBUG",
+            "class": "apps.api.logging.TerraConsoleHandler",
+            "formatter": "terra.console",
+        },
+        "terra.request_catcher": {
+            "level": "DEBUG",
+            "class": "apps.api.logging.TerraLogsCatcherHandler",
+            "formatter": "terra.console",
+        },
+        "mail_admins": {
+            "level": "ERROR",
+            "filters": ["require_debug_false"],
+            "class": "django.utils.log.AdminEmailHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console", "mail_admins"],
+            "level": "INFO",
+        },
+        "django.server": {
+            "handlers": ["django.server"],
+            "level": "INFO",
+            "propagate": False,
+        },
+        "terra": {
+            "handlers": ["terra.console", "terra.request_catcher"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+    },
+}
