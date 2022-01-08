f7b54c0da8aefeef3646e9ffeb222f0e216c0743
Deny <35976671+SurriKen@users.noreply.github.com>
2022-01-08 20:59:24 +0300

    -

--- terra_ai/modeling/validator.py
+++ terra_ai/modeling/validator.py
@@ -171,7 +171,7 @@ class ModelValidator:
     def get_validated(self):
         """Returns all necessary info about modeling"""
         # logger.debug(f"{self.name}, {self.get_validated.__name__}")
-        logger.info("Валидация модели...", extra={'type': "info"})
+        logger.info("Валидация модели...")
         self._model_validation()
         if self.valid:
             self.compile_keras_code()
