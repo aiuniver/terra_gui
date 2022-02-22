9600c63208ba77dcdbcc7b4537761e0cb45de429
arthurkazaryan <arthur.kazaryan@yahoo.com>
2022-02-22 20:57:57 +0300

    Добавил импорт модулей arrays_classes

---
+++ terra_ai/datasets/arrays_classes/__init__.py
@@ -0,0 +1,21 @@
+from . import \
+    audio, \
+    classification, \
+    discriminator, \
+    generator, \
+    image, \
+    noise, \
+    raw, \
+    regression, \
+    scaler, \
+    segmentation, \
+    speech_2_text, \
+    text, \
+    text_2_speech, \
+    text_segmentation, \
+    timeseries, \
+    timeseries_trend, \
+    tracker, \
+    video, \
+    yolo_v3, \
+    yolo_v4
